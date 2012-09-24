1812ad4d1d42f73c183def4dc6c672f9f1810354
Ondřej Čertík <ondrej.certik@gmail.com>
2012-09-23 18:30:22 -0700

    test_fm: always test up to m=500
    The Fm() function is now more precise.

--- src/tests/test_fm.f90
+++ src/tests/test_fm.f90
@@ -8,33 +8,20 @@ implicit none
 real(dp), allocatable :: F(:), G(:)
 real(dp) :: x
 real(dp), parameter :: eps = 1e-15_dp
-real(dp), parameter :: xlist_high(*) = [1e-6_dp, 0.1_dp, 0.5_dp, 2._dp, &
-    10._dp, 15._dp, 19._dp, 200._dp, 500._dp]
-real(dp), parameter :: xlist_low(*) = [20._dp, 21._dp, 25._dp, 30._dp, &
-    50._dp, 100._dp, 150._dp]
+real(dp), parameter :: xlist(*) = [1e-6_dp, 0.1_dp, 0.5_dp, 2._dp, 10._dp, &
+    15._dp, 19._dp, 20._dp, 21._dp, 25._dp, 30._dp, 50._dp, 100._dp, 150._dp, &
+    200._dp, 500._dp]
 integer :: m, maxm, i
 ! We use downward recursion with initial value 0, so maxm must be very high to
 ! get accurate results:
 maxm = 1000
 allocate(F(0:maxm), G(0:maxm))
 
-do i = 1, size(xlist_high)
-    x = xlist_high(i)
+do i = 1, size(xlist)
+    x = xlist(i)
     call downwards(maxm, x, F)
     call assert(abs(F(0) - 0.5_dp * sqrt(pi/x) * erf(sqrt(x))) < eps)
-    ! With these "x" we can go all the way to m=500 with high accuracy:
     do m = 0, 500
-        call Fm(m, x, G(:m))
-        call assert(all(abs(G(:m) - F(:m)) < eps))
-    end do
-end do
-
-do i = 1, size(xlist_low)
-    x = xlist_low(i)
-    call downwards(maxm, x, F)
-    call assert(abs(F(0) - 0.5_dp * sqrt(pi/x) * erf(sqrt(x))) < eps)
-    ! With these "x" we can only go to m=50 with high accuracy:
-    do m = 0, 50
         call Fm(m, x, G(:m))
         call assert(all(abs(G(:m) - F(:m)) < eps))
     end do
