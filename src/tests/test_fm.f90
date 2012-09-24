c93fc230d2ce77d07aa276a36eeec3e78b7f5071
Ondřej Čertík <ondrej.certik@gmail.com>
2012-09-23 18:56:30 -0700

    For large "t", compare against asymptotic formula

--- src/tests/test_fm.f90
+++ src/tests/test_fm.f90
@@ -10,7 +10,7 @@ real(dp) :: x
 real(dp), parameter :: eps = 1e-15_dp
 real(dp), parameter :: xlist(*) = [1e-6_dp, 0.1_dp, 0.5_dp, 2._dp, 10._dp, &
     15._dp, 19._dp, 20._dp, 21._dp, 25._dp, 30._dp, 50._dp, 100._dp, 150._dp, &
-    200._dp, 500._dp]
+    200._dp, 500._dp, 2000._dp, 20000._dp, 2e5_dp]
 integer :: m, maxm, i
 ! We use downward recursion with initial value 0, so maxm must be very high to
 ! get accurate results:
@@ -19,7 +19,11 @@ allocate(F(0:maxm), G(0:maxm))
 
 do i = 1, size(xlist)
     x = xlist(i)
-    call downwards(maxm, x, F)
+    if (x < 510) then
+        call downwards(maxm, x, F)
+    else
+        call asympt(maxm, x, F)
+    end if
     call assert(abs(F(0) - 0.5_dp * sqrt(pi/x) * erf(sqrt(x))) < eps)
     do m = 0, 500
         call Fm(m, x, G(:m))
@@ -37,6 +41,19 @@ integer :: m
 F(maxm) = 0
 do m = maxm-1, 0, -1
     F(m) = (2*t*F(m + 1) + exp(-t)) / (2*m + 1)
+end do
+end subroutine
+
+subroutine asympt(maxm, t, F)
+integer, intent(in) :: maxm
+real(dp), intent(in) :: t
+real(dp), intent(out) :: F(0:)
+integer :: m
+do m = 0, maxm
+    ! This can overflow (NaN):
+    !F(m) = gamma(m+0.5_dp) / (2*t**(m+0.5_dp))
+    ! so this is equivalent, but works for large "t" and "m":
+    F(m) = exp(log_gamma(m+0.5_dp) - (m+0.5_dp)*log(t)) / 2
 end do
 end subroutine
 
