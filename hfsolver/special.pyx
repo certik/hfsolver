44d9df21618e17dd61889edf45ad003255bdd441
Ondřej Čertík <ondrej.certik@gmail.com>
2012-10-05 15:07:26 -0700

    Implement Knu

--- hfsolver/special.pyx
+++ hfsolver/special.pyx
@@ -4,3 +4,8 @@ def Inu(int k, double x):
     cdef double r
     c_hfsolver.hfsolver_Inu(&k, &x, &r)
     return r
+
+def Knu(int k, double x):
+    cdef double r
+    c_hfsolver.hfsolver_Knu(&k, &x, &r)
+    return r
