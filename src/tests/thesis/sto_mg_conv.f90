5be10dcb07d1ebeaa60debee571befa1f8b0d7d0
Ondřej Čertík <ondrej.certik@gmail.com>
2012-10-11 16:01:53 -0700

    Refactoring

--- src/tests/thesis/sto_mg_conv.f90
+++ src/tests/thesis/sto_mg_conv.f90
@@ -1,3 +1,31 @@
+module sto_mg_basis
+use types, only: dp
+use utils, only: assert
+implicit none
+private
+public sto_optimized
+
+contains
+
+subroutine sto_optimized(Lmax, nbfl, nl, zl)
+integer, intent(in) :: Lmax
+integer, allocatable, intent(out) :: nl(:, :), nbfl(:)
+real(dp), allocatable, intent(out) :: zl(:, :)
+call assert(Lmax == 1)
+allocate(nbfl(0:Lmax))
+nbfl = [10, 7]
+allocate(nl(maxval(nbfl), 0:Lmax), zl(maxval(nbfl), 0:Lmax))
+nl(:nbfl(0), 0) = [2, 1, 2, 1, 1, 2, 1, 1, 2, 1]
+zl(:nbfl(0), 0) = [35.988252_dp, 19.193253_dp, 16.093419_dp, 10.773377_dp, &
+                7.853579_dp, 6.023976_dp, 2.905152_dp, 1.694174_dp, &
+                0.750076_dp, 0.711410_dp]
+nl(:nbfl(1), 1) = [3, 2, 3, 2, 2, 2, 2]
+zl(:nbfl(1), 1) = [32.077221_dp, 12.495413_dp, 9.644163_dp, 5.042248_dp, &
+                3.070882_dp, 2.086080_dp, 0.711410_dp]
+end subroutine
+
+end module
+
 program sto_mg_conv
 
 ! Even-tempered (ET) STO basis convergence for Mg.
@@ -14,30 +42,23 @@ use mbpt, only: transform_int2, mbpt2, mbpt3, mbpt4, transform_int22
 use gf2, only: find_poles, find_pole_diag, total_energy, plot_poles
 use sorting, only: argsort
 use scf, only: ijkl2intindex, ijkl2intindex2, create_intindex_sym4
+use sto_mg_basis, only: sto_optimized
 implicit none
 
 integer, allocatable :: nl(:, :), nbfl(:)
 real(dp), allocatable :: zl(:, :), focc(:, :)
-
 real(dp), allocatable :: S(:, :, :), T(:, :, :), V(:, :, :), slater(:, :)
 integer :: n, Z, m, Nscf, Lmax, ndof
 real(dp) :: alpha, Etot, tolE, tolP, Ekin
 real(dp), allocatable :: H(:, :, :), P_(:, :, :), C(:, :, :), lam(:, :)
+!real(dp), allocatable :: alpha(:), beta(:)
 
 Lmax = 1
-allocate(nbfl(0:Lmax))
-nbfl = [10, 7]
-allocate(nl(maxval(nbfl), 0:Lmax), zl(maxval(nbfl), 0:Lmax), focc(3, 0:Lmax))
+allocate(focc(3, 0:Lmax))
 focc = 0
 focc(:3, 0) = [2, 2, 2]
 focc(:1, 1) = [6]
-nl(:nbfl(0), 0) = [2, 1, 2, 1, 1, 2, 1, 1, 2, 1]
-zl(:nbfl(0), 0) = [35.988252_dp, 19.193253_dp, 16.093419_dp, 10.773377_dp, &
-                7.853579_dp, 6.023976_dp, 2.905152_dp, 1.694174_dp, &
-                0.750076_dp, 0.711410_dp]
-nl(:nbfl(1), 1) = [3, 2, 3, 2, 2, 2, 2]
-zl(:nbfl(1), 1) = [32.077221_dp, 12.495413_dp, 9.644163_dp, 5.042248_dp, &
-                3.070882_dp, 2.086080_dp, 0.711410_dp]
+call sto_optimized(Lmax, nbfl, nl, zl)
 
 Z = 12
 tolE = 1e-10_dp
