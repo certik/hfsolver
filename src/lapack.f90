fc038b5eedbe76614a8b0eb0f8356db76e2d0dc2
Ondřej Čertík <ondrej.certik@gmail.com>
2012-10-12 23:27:56 -0700

    Update lapack and linalg

--- src/lapack.f90
+++ src/lapack.f90
@@ -1,7 +1,5 @@
-module lapack_precision
+module lapack
 implicit none
-private
-public dp
 
 ! This is the precision that LAPACK "d" routines were compiled with (typically
 ! double precision, unless a special compiler option was used while compiling
@@ -13,32 +11,56 @@ public dp
 ! quadruple precision, it will be promoted both in LAPACK and here.)
 integer, parameter:: dp=kind(0.d0)
 
-end module
-
-
-
-
-module lapack
-implicit none
-
 interface
 
     SUBROUTINE DGESV( N, NRHS, A, LDA, IPIV, B, LDB, INFO )
-    use lapack_precision, only: dp
+    import :: dp
     INTEGER            INFO, LDA, LDB, N, NRHS
     INTEGER            IPIV( * )
     REAL(dp)           A( LDA, * ), B( LDB, * )
     END SUBROUTINE
 
+    SUBROUTINE DGESVX( FACT, TRANS, N, NRHS, A, LDA, AF, LDAF, IPIV, &
+                       EQUED, R, C, B, LDB, X, LDX, RCOND, FERR, BERR, WORK, &
+                       IWORK, INFO )
+    import :: dp
+    CHARACTER          EQUED, FACT, TRANS
+    INTEGER            INFO, LDA, LDAF, LDB, LDX, N, NRHS
+    REAL(dp)           RCOND
+    INTEGER            IPIV( * ), IWORK( * )
+    REAL(dp)           A( LDA, * ), AF( LDAF, * ), B( LDB, * ), BERR( * ), &
+                       C( * ), FERR( * ), R( * ), WORK( * ), X( LDX, * )
+    END SUBROUTINE
+
+    SUBROUTINE ZGESV( N, NRHS, A, LDA, IPIV, B, LDB, INFO )
+    import :: dp
+    INTEGER            INFO, LDA, LDB, N, NRHS
+    INTEGER            IPIV( * )
+    COMPLEX(dp)        A( LDA, * ), B( LDB, * )
+    END SUBROUTINE
+
+    SUBROUTINE ZGESVX( FACT, TRANS, N, NRHS, A, LDA, AF, LDAF, IPIV, &
+                       EQUED, R, C, B, LDB, X, LDX, RCOND, FERR, BERR, &
+                       WORK, RWORK, INFO )
+    import :: dp
+    CHARACTER          EQUED, FACT, TRANS
+    INTEGER            INFO, LDA, LDAF, LDB, LDX, N, NRHS
+    REAL(dp)           RCOND
+    INTEGER            IPIV( * )
+    REAL(dp)           BERR( * ), C( * ), FERR( * ), R( * ), RWORK( * )
+    COMPLEX(dp)        A( LDA, * ), AF( LDAF, * ), B( LDB, * ), WORK( * ), &
+                       X( LDX, * )
+    END SUBROUTINE
+
     SUBROUTINE DGBSV( N, KL, KU, NRHS, AB, LDAB, IPIV, B, LDB, INFO )
-    use lapack_precision, only: dp
+    import :: dp
     INTEGER            INFO, KL, KU, LDAB, LDB, N, NRHS
     INTEGER            IPIV( * )
     REAL(dp)           AB( LDAB, * ), B( LDB, * )
     END SUBROUTINE
 
     SUBROUTINE DSYSV( UPLO, N, NRHS, A, LDA, IPIV, B, LDB, WORK, LWORK, INFO )
-    use lapack_precision, only: dp
+    import :: dp
     CHARACTER          UPLO
     INTEGER            INFO, LDA, LDB, LWORK, N, NRHS
     INTEGER            IPIV( * )
@@ -46,9 +68,9 @@ interface
     END SUBROUTINE
 
     SUBROUTINE DSYSVX( FACT, UPLO, N, NRHS, A, LDA, AF, LDAF, IPIV, B, &
-        LDB, X, LDX, RCOND, FERR, BERR, WORK, LWORK, &
-        IWORK, INFO )
-    use lapack_precision, only: dp
+                       LDB, X, LDX, RCOND, FERR, BERR, WORK, LWORK, &
+                       IWORK, INFO )
+    import :: dp
     CHARACTER          FACT, UPLO
     INTEGER            INFO, LDA, LDAF, LDB, LDX, LWORK, N, NRHS
     REAL(dp)           RCOND
@@ -57,10 +79,19 @@ interface
                        BERR( * ), FERR( * ), WORK( * ), X( LDX, * )
     END SUBROUTINE
 
+    SUBROUTINE DSYEVD( JOBZ, UPLO, N, A, LDA, W, WORK, LWORK, IWORK, &
+                       LIWORK, INFO )
+    import :: dp
+    CHARACTER          JOBZ, UPLO
+    INTEGER            INFO, LDA, LIWORK, LWORK, N
+    INTEGER            IWORK( * )
+    REAL(dp)           A( LDA, * ), W( * ), WORK( * )
+    END SUBROUTINE
+
     SUBROUTINE DSYGVX( ITYPE, JOBZ, RANGE, UPLO, N, A, LDA, B, LDB, &
                        VL, VU, IL, IU, ABSTOL, M, W, Z, LDZ, WORK, &
                        LWORK, IWORK, IFAIL, INFO )
-    use lapack_precision, only: dp
+    import :: dp
     CHARACTER          JOBZ, RANGE, UPLO
     INTEGER            IL, INFO, ITYPE, IU, LDA, LDB, LDZ, LWORK, M, N
     REAL(dp)           ABSTOL, VL, VU
@@ -70,8 +101,8 @@ interface
     END SUBROUTINE
 
     SUBROUTINE DGGEV( JOBVL, JOBVR, N, A, LDA, B, LDB, ALPHAR, ALPHAI, &
-        BETA, VL, LDVL, VR, LDVR, WORK, LWORK, INFO )
-    use lapack_precision, only: dp
+                      BETA, VL, LDVL, VR, LDVR, WORK, LWORK, INFO )
+    import :: dp
     CHARACTER          JOBVL, JOBVR
     INTEGER            INFO, LDA, LDB, LDVL, LDVR, LWORK, N
     REAL(dp)           A( LDA, * ), ALPHAI( * ), ALPHAR( * ), &
@@ -79,9 +110,68 @@ interface
                        VR( LDVR, * ), WORK( * )
     END SUBROUTINE
 
+    SUBROUTINE DGGEVX( BALANC, JOBVL, JOBVR, SENSE, N, A, LDA, B, LDB, &
+                       ALPHAR, ALPHAI, BETA, VL, LDVL, VR, LDVR, ILO, IHI, &
+                       LSCALE, RSCALE, ABNRM, BBNRM, RCONDE, RCONDV, WORK, &
+                       LWORK, IWORK, BWORK, INFO )
+    import :: dp
+    CHARACTER          BALANC, JOBVL, JOBVR, SENSE
+    INTEGER            IHI, ILO, INFO, LDA, LDB, LDVL, LDVR, LWORK, N
+    REAL(dp)           ABNRM, BBNRM
+    LOGICAL            BWORK( * )
+    INTEGER            IWORK( * )
+    REAL(dp)           A( LDA, * ), ALPHAI( * ), ALPHAR( * ), B( LDB, * ), &
+                       BETA( * ), LSCALE( * ), RCONDE( * ), RCONDV( * ), &
+                       RSCALE( * ), VL( LDVL, * ), VR( LDVR, * ), WORK( * )
+    END SUBROUTINE
+
+    SUBROUTINE DGEEV( JOBVL, JOBVR, N, A, LDA, WR, WI, VL, LDVL, VR, &
+                      LDVR, WORK, LWORK, INFO )
+    import :: dp
+    CHARACTER          JOBVL, JOBVR
+    INTEGER            INFO, LDA, LDVL, LDVR, LWORK, N
+    REAL(dp)           A( LDA, * ), VL( LDVL, * ), VR( LDVR, * ), WI( * ), &
+                       WORK( * ), WR( * )
+    END SUBROUTINE
+
+    SUBROUTINE DGEEVX( BALANC, JOBVL, JOBVR, SENSE, N, A, LDA, WR, WI, &
+                       VL, LDVL, VR, LDVR, ILO, IHI, SCALE, ABNRM, &
+                       RCONDE, RCONDV, WORK, LWORK, IWORK, INFO )
+    import :: dp
+    CHARACTER          BALANC, JOBVL, JOBVR, SENSE
+    INTEGER            IHI, ILO, INFO, LDA, LDVL, LDVR, LWORK, N
+    REAL(dp)           ABNRM
+    INTEGER            IWORK( * )
+    REAL(dp)           A( LDA, * ), RCONDE( * ), RCONDV( * ), &
+                       SCALE( * ), VL( LDVL, * ), VR( LDVR, * ), &
+                       WI( * ), WORK( * ), WR( * )
+    END SUBROUTINE
+
+    SUBROUTINE ZGEEV( JOBVL, JOBVR, N, A, LDA, W, VL, LDVL, VR, LDVR, &
+                      WORK, LWORK, RWORK, INFO )
+    import :: dp
+    CHARACTER          JOBVL, JOBVR
+    INTEGER            INFO, LDA, LDVL, LDVR, LWORK, N
+    REAL(dp)           RWORK( * )
+    COMPLEX(dp)        A( LDA, * ), VL( LDVL, * ), VR( LDVR, * ), W( * ), &
+                       WORK( * )
+    END SUBROUTINE
+
+    SUBROUTINE ZGEEVX( BALANC, JOBVL, JOBVR, SENSE, N, A, LDA, W, VL, &
+                       LDVL, VR, LDVR, ILO, IHI, SCALE, ABNRM, RCONDE, &
+                       RCONDV, WORK, LWORK, RWORK, INFO )
+    import :: dp
+    CHARACTER          BALANC, JOBVL, JOBVR, SENSE
+    INTEGER            IHI, ILO, INFO, LDA, LDVL, LDVR, LWORK, N
+    REAL(dp)           ABNRM
+    REAL(dp)           RCONDE( * ), RCONDV( * ), RWORK( * ), SCALE( * )
+    COMPLEX(dp)        A( LDA, * ), VL( LDVL, * ), VR( LDVR, * ), W( * ), &
+                       WORK( * )
+    END SUBROUTINE
+
     SUBROUTINE DSYGVD( ITYPE, JOBZ, UPLO, N, A, LDA, B, LDB, W, WORK, &
-        LWORK, IWORK, LIWORK, INFO )
-    use lapack_precision, only: dp
+                       LWORK, IWORK, LIWORK, INFO )
+    import :: dp
     CHARACTER          JOBZ, UPLO
     INTEGER            INFO, ITYPE, LDA, LDB, LIWORK, LWORK, N
     INTEGER            IWORK( * )
@@ -89,7 +179,7 @@ interface
     END SUBROUTINE
 
     REAL(dp) FUNCTION DLAMCH( CMACH )
-    use lapack_precision, only: dp
+    import :: dp
     CHARACTER          CMACH
     END FUNCTION
 
@@ -99,17 +189,97 @@ interface
     END FUNCTION
 
     SUBROUTINE ZGETRF( M, N, A, LDA, IPIV, INFO )
-    use lapack_precision, only: dp
+    import :: dp
     INTEGER            INFO, LDA, M, N
     INTEGER            IPIV( * )
     COMPLEX(dp)        A( LDA, * )
     END SUBROUTINE
 
     SUBROUTINE ZGETRI( N, A, LDA, IPIV, WORK, LWORK, INFO )
-    use lapack_precision, only: dp
+    import :: dp
     INTEGER            INFO, LDA, LWORK, N
     INTEGER            IPIV( * )
     COMPLEX(dp)        A( LDA, * ), WORK( * )
+    END SUBROUTINE
+
+    SUBROUTINE DGETRF( M, N, A, LDA, IPIV, INFO )
+    import :: dp
+    INTEGER            INFO, LDA, M, N
+    INTEGER            IPIV( * )
+    REAL(dp)           A( LDA, * )
+    END SUBROUTINE
+
+    SUBROUTINE DGETRI( N, A, LDA, IPIV, WORK, LWORK, INFO )
+    import :: dp
+    INTEGER            INFO, LDA, LWORK, N
+    INTEGER            IPIV( * )
+    REAL(dp)           A( LDA, * ), WORK( * )
+    END SUBROUTINE
+
+    SUBROUTINE ZHEEV( JOBZ, UPLO, N, A, LDA, W, WORK, LWORK, RWORK, INFO )
+    import :: dp
+    CHARACTER          JOBZ, UPLO
+    INTEGER            INFO, LDA, LWORK, N
+    REAL(dp)           RWORK( * ), W( * )
+    COMPLEX(dp)        A( LDA, * ), WORK( * )
+    END SUBROUTINE
+
+    SUBROUTINE ZHEEVD( JOBZ, UPLO, N, A, LDA, W, WORK, LWORK, RWORK, &
+                       LRWORK, IWORK, LIWORK, INFO )
+    import :: dp
+    CHARACTER          JOBZ, UPLO
+    INTEGER            INFO, LDA, LIWORK, LRWORK, LWORK, N
+    INTEGER            IWORK( * )
+    REAL(dp)           RWORK( * ), W( * )
+    COMPLEX(dp)        A( LDA, * ), WORK( * )
+    END SUBROUTINE
+
+    SUBROUTINE ZHEGVD( ITYPE,  JOBZ,  UPLO,  N,  A,  LDA,  B, LDB, W, &
+                       WORK, LWORK, RWORK, LRWORK, IWORK, LIWORK, &
+                       INFO )
+    import :: dp
+    CHARACTER          JOBZ, UPLO
+    INTEGER            INFO, ITYPE, LDA, LDB, LIWORK, LRWORK, LWORK, N
+    INTEGER            IWORK( * )
+    REAL(dp)           RWORK( * ), W( * )
+    COMPLEX(dp)        A( LDA, * ), B( LDB, * ), WORK( * )
+    END SUBROUTINE
+
+    SUBROUTINE DGELSY( M, N, NRHS, A, LDA, B, LDB, JPVT, RCOND, RANK, &
+                       WORK, LWORK, INFO )
+    import :: dp
+    INTEGER            INFO, LDA, LDB, LWORK, M, N, NRHS, RANK
+    REAL(dp)           RCOND
+    INTEGER            JPVT( * )
+    REAL(dp)           A( LDA, * ), B( LDB, * ), WORK( * )
+    END SUBROUTINE
+
+    SUBROUTINE ZGELSY( M, N, NRHS, A, LDA, B, LDB, JPVT, RCOND, RANK, &
+                       WORK, LWORK, RWORK, INFO )
+    import :: dp
+    INTEGER            INFO, LDA, LDB, LWORK, M, N, NRHS, RANK
+    REAL(dp)           RCOND
+    INTEGER            JPVT( * )
+    REAL(dp)           RWORK( * )
+    COMPLEX(dp)        A( LDA, * ), B( LDB, * ), WORK( * )
+    END SUBROUTINE
+
+    SUBROUTINE DGESVD( JOBU, JOBVT, M, N, A, LDA, S, U, LDU, VT, &
+                       LDVT, WORK, LWORK, INFO )
+    import :: dp
+    CHARACTER          JOBU, JOBVT
+    INTEGER            INFO, LDA, LDU, LDVT, LWORK, M, N
+    REAL(dp)           A( LDA, * ), S( * ),  U( LDU,  * ), VT( LDVT, * ), &
+                       WORK( * )
+    END SUBROUTINE
+
+    SUBROUTINE ZGESVD( JOBU, JOBVT,  M,  N,  A,  LDA, S, U, LDU, VT, LDVT, &
+                       WORK, LWORK, RWORK, INFO )
+    import :: dp
+    CHARACTER          JOBU, JOBVT
+    INTEGER            INFO, LDA, LDU, LDVT, LWORK, M, N
+    REAL(dp)           RWORK( * ), S( * )
+    COMPLEX(dp)        A( LDA, * ), U( LDU, * ), VT( LDVT, * ), WORK( * )
     END SUBROUTINE
 
 end interface
