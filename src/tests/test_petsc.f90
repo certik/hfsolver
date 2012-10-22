ced7ca56819526305f4ac238644446aecf6dcb60
Ondřej Čertík <ondrej.certik@gmail.com>
2012-10-22 07:19:33 -0700

    Simplify test_petsc.f90

--- src/tests/test_petsc.f90
+++ src/tests/test_petsc.f90
@@ -9,15 +9,9 @@ complex(dp) :: A(n, n), b(n), x(n)
 integer :: i, ierr
 
 A = 0
-A(1, 1) = 2
-A(1, 2) = -1
-do i = 2, n-1
-    A(i, i-1) = -1
-    A(i, i) = 2
-    A(i, i+1) = -1
-end do
-A(n, n-1) = -1
-A(n, n) = 2
+A(1, 1:2) = [2, -1]
+forall(i = 2:n-1) A(i, i-1:i+1) = [-1, 2, -1]
+A(n, n-1:n) = [-1, 2]
 
 x = 1+2*i_
 b = matmul(A, x)
