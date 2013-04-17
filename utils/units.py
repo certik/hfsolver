a682eaf7becf5970fdb0c801898c4713cb0d17a9
Ondřej Čertík <ondrej.certik@gmail.com>
2013-04-17 17:03:30 -0600

    Add references to units

--- utils/units.py
+++ utils/units.py
@@ -1,9 +1,10 @@
 # SI units:
-e = 1.602176565e-19     # C
-kB = 1.3806488e-23      # J/K
+e = 1.602176565e-19     # C, 2010 CODATA value
+kB = 1.3806488e-23      # J/K, 2010 CODATA value
 eV = 1 * e                  # V * C = J/C * C = J
 # For temperature, 1 eV/kB in K:
 eV2K = eV / kB    # J / (J/K) = K
 
+print "1 eV/kB =", eV2K, "K"
 print "1e5 K =", 1e5 / eV2K, "eV"
 print "15 keV =", 15e3 * eV2K / 1e6, "MK"
