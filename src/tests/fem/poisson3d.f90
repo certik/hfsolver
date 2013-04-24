36d7406d81911647217ea642456b8159566b166a
Ondřej Čertík <ondrej.certik@gmail.com>
2013-04-23 22:30:59 -0600

    Update the solver to use 3D

--- src/tests/fem/poisson3d.f90
+++ src/tests/fem/poisson3d.f90
@@ -5,85 +5,106 @@ use utils, only: assert, stop_error
 use constants, only: pi
 implicit none
 private
-public assemble_2d, sol_error
+public assemble_3d, sol_error
 
 contains
 
-real(dp) elemental function f(x, y)
-real(dp), intent(in) :: x, y
-f = 2 * pi**2 * sin(pi*x) * sin(pi*y)
+real(dp) elemental function f(x, y, z)
+real(dp), intent(in) :: x, y, z
+f = 3 * pi**2 * sin(pi*x) * sin(pi*y) * sin(pi*z)
 end function
 
-real(dp) elemental function exact_sol(x, y)
-real(dp), intent(in) :: x, y
-exact_sol = sin(pi*x) * sin(pi*y)
+real(dp) elemental function exact_sol(x, y, z)
+real(dp), intent(in) :: x, y, z
+exact_sol = sin(pi*x) * sin(pi*y) * sin(pi*z)
 end function
 
-subroutine assemble_2d(xin, nodes, elems, ib, xiq, wtq, phihq, dphihq, Am, rhs)
+subroutine assemble_3d(xin, nodes, elems, ib, xiq, wtq, phihq, dphihq, Am, rhs)
 ! Assemble on a 2D rectangular uniform mesh
-real(dp), intent(in):: xin(:), nodes(:, :), xiq(:), wtq(:, :), &
+real(dp), intent(in):: xin(:), nodes(:, :), xiq(:), wtq(:, :, :), &
     phihq(:, :), dphihq(:, :)
-integer, intent(in):: elems(:, :), ib(:, :, :)
+integer, intent(in):: elems(:, :), ib(:, :, :, :)
 real(dp), intent(out):: Am(:,:), rhs(:)
-integer :: Ne, p, e, i, j, iqx, iqy
-real(dp), dimension(size(xiq), size(xiq), size(xin), size(xin)) :: &
-    phi_v, phi_dx, phi_dy
-real(dp) :: x(size(xiq)), y(size(xiq))
-real(dp) :: fq(size(xiq), size(xiq)), xp(size(xiq)), yp(size(xiq))
-real(dp) :: lx, ly
-integer :: ax, ay, bx, by
-real(dp) :: jacx, jacy, jac_det
+integer :: Ne, p, e, i, j, iqx, iqy, iqz
+real(dp), dimension(size(xiq), size(xiq), size(xiq), &
+    size(xin), size(xin), size(xin)) :: phi_v, phi_dx, phi_dy, phi_dz
+real(dp), dimension(size(xiq), size(xiq), size(xiq)) :: fq
+real(dp), dimension(size(xiq)) :: x, y, z, xp, yp, zp
+real(dp) :: lx, ly, lz
+integer :: ax, ay, az, bx, by, bz
+real(dp) :: jacx, jacy, jacz, jac_det
 
 Ne = size(elems, 2)
 p = size(xin) - 1
-! 2D shape functions
+! 3D shape functions
+do az = 1, p+1
 do ay = 1, p+1
-    do ax = 1, p+1
-        do iqy = 1, size(xiq)
-            do iqx = 1, size(xiq)
-                phi_v(iqx, iqy, ax, ay) = phihq(iqx, ax)*phihq(iqy, ay)
-                phi_dx(iqx, iqy, ax, ay) = dphihq(iqx, ax)*phihq(iqy, ay)
-                phi_dy(iqx, iqy, ax, ay) = phihq(iqx, ax)*dphihq(iqy, ay)
-            end do
-        end do
-   end do
+do ax = 1, p+1
+    do iqz = 1, size(xiq)
+    do iqy = 1, size(xiq)
+    do iqx = 1, size(xiq)
+        phi_v (iqx, iqy, iqz, ax, ay, az) = &
+             phihq(iqx, ax) *  phihq(iqy, ay) *  phihq(iqz, az)
+        phi_dx(iqx, iqy, iqz, ax, ay, az) = &
+            dphihq(iqx, ax) *  phihq(iqy, ay) *  phihq(iqz, az)
+        phi_dy(iqx, iqy, iqz, ax, ay, az) = &
+             phihq(iqx, ax) * dphihq(iqy, ay) *  phihq(iqz, az)
+        phi_dz(iqx, iqy, iqz, ax, ay, az) = &
+             phihq(iqx, ax) *  phihq(iqy, ay) * dphihq(iqz, az)
+    end do
+    end do
+    end do
+end do
+end do
 end do
 Am=0; rhs=0
 ! Precalculate as much as possible:
-lx = nodes(1, elems(3, 1)) - nodes(1, elems(1, 1)) ! Element sizes
-ly = nodes(2, elems(3, 1)) - nodes(2, elems(1, 1))
+lx = nodes(1, elems(7, 1)) - nodes(1, elems(1, 1)) ! Element sizes
+ly = nodes(2, elems(7, 1)) - nodes(2, elems(1, 1))
+lz = nodes(3, elems(7, 1)) - nodes(3, elems(1, 1))
 jacx = lx/2
 jacy = ly/2
-jac_det = abs(jacx*jacy)
+jacz = lz/2
+jac_det = abs(jacx*jacy*jacz)
 xp = (xiq + 1) * jacx
 yp = (xiq + 1) * jacy
+zp = (xiq + 1) * jacz
 phi_dx = phi_dx / jacx
 phi_dy = phi_dy / jacy
+phi_dz = phi_dz / jacz
 do e = 1, Ne
     x = xp + nodes(1, elems(1, e))
     y = yp + nodes(2, elems(1, e))
+    z = zp + nodes(3, elems(1, e))
+    do iqz = 1, size(xiq)
     do iqy = 1, size(xiq)
-        do iqx = 1, size(xiq)
-            fq(iqx, iqy) = f(x(iqx), y(iqy))
-        end do
+    do iqx = 1, size(xiq)
+        fq(iqx, iqy, iqz) = f(x(iqx), y(iqy), z(iqz))
+    end do
+    end do
     end do
     fq = fq * jac_det * wtq
+    do bz = 1, p+1
     do by = 1, p+1
     do bx = 1, p+1
-        j = ib(bx, by, e)
+        j = ib(bx, by, bz, e)
         if (j==0) cycle
+        do az = 1, p+1
         do ay = 1, p+1
         do ax = 1, p+1
-            i = ib(ax, ay, e)
+            i = ib(ax, ay, az, e)
             if (i == 0) cycle
             if (j > i) cycle
             Am(i,j) = Am(i,j) + sum( &
-                    (phi_dx(:, :, ax, ay)*phi_dx(:, :, bx, by) &
-                    +phi_dy(:, :, ax, ay)*phi_dy(:, :, bx, by)) &
-                * jac_det * wtq)
+                (phi_dx(:, :, :, ax, ay, az)*phi_dx(:, :, :, bx, by, bz) &
+                +phi_dy(:, :, :, ax, ay, az)*phi_dy(:, :, :, bx, by, bz) &
+                +phi_dz(:, :, :, ax, ay, az)*phi_dz(:, :, :, bx, by, bz)) &
+                    * jac_det * wtq)
         end do
         end do
-        rhs(j) = rhs(j) + sum(phi_v(:, :, bx, by) * fq)
+        end do
+        rhs(j) = rhs(j) + sum(phi_v(:, :, :, bx, by, bz) * fq)
+    end do
     end do
     end do
 end do
@@ -97,27 +118,32 @@ end subroutine
 real(dp) function sol_error(nodes, elems, xiq, wtq, solq) result(r)
 real(dp), intent(in) :: nodes(:, :)
 integer, intent(in) :: elems(:, :)
-real(dp), intent(in) :: xiq(:), wtq(:,:), solq(:, :, :)
-real(dp) :: fq(size(xiq), size(xiq)), x(size(xiq)), y(size(xiq))
-real(dp) :: jacx, jacy, l(2), jac_det
-integer :: e, iqx, iqy, Ne
+real(dp), intent(in) :: xiq(:), wtq(:, :, :), solq(:, :, :, :)
+real(dp) :: fq(size(xiq), size(xiq), size(xiq)), x(size(xiq)), y(size(xiq)), &
+    z(size(xiq))
+real(dp) :: jacx, jacy, jacz, l(3), jac_det
+integer :: e, iqx, iqy, iqz, Ne
 Ne = size(elems, 2)
 r = 0
 do e = 1, Ne
     ! l is the diagonal vector:
-    l = nodes(:, elems(3, e)) - nodes(:, elems(1, e))
+    l = nodes(:, elems(7, e)) - nodes(:, elems(1, e))
     ! Assume rectangular shape:
     jacx = l(1)/2
     jacy = l(2)/2
-    jac_det = abs(jacx*jacy)
+    jacz = l(3)/2
+    jac_det = abs(jacx*jacy*jacz)
     x = (xiq + 1) * jacx + nodes(1, elems(1, e))
     y = (xiq + 1) * jacy + nodes(2, elems(1, e))
+    z = (xiq + 1) * jacz + nodes(3, elems(1, e))
+    do iqz = 1, size(xiq)
     do iqy = 1, size(xiq)
-        do iqx = 1, size(xiq)
-            fq(iqx, iqy) = exact_sol(x(iqx), y(iqy))
-        end do
+    do iqx = 1, size(xiq)
+            fq(iqx, iqy, iqz) = exact_sol(x(iqx), y(iqy), z(iqz))
     end do
-    fq = fq-solq(:, :, e)
+    end do
+    end do
+    fq = fq-solq(:, :, :, e)
     r = r + sum(fq*fq * jac_det * wtq)
 end do
 r = sqrt(r)
@@ -131,9 +157,9 @@ module poisson3d_code
 
 use types, only: dp
 use feutils, only: phih, dphih
-use fe_mesh, only: cartesian_mesh_2d, cartesian_mesh_3d, &
-    define_connect_tensor_2d, c2fullc_2d, fe2quad_2d
-use poisson3d_assembly, only: assemble_2d, sol_error
+use fe_mesh, only: cartesian_mesh_3d, define_connect_tensor_3d, &
+    c2fullc_3d, fe2quad_3d
+use poisson3d_assembly, only: assemble_3d, sol_error
 use feutils, only: get_parent_nodes, get_parent_quad_pts_wts
 use linalg, only: solve
 use constants, only: pi
@@ -141,7 +167,7 @@ implicit none
 
 contains
 
-real(dp) function solve_poisson(Nex, Ney, p) result(error)
+real(dp) function solve_poisson(Nex, Ney, Nez, p) result(error)
 integer, intent(in) :: p
 
 integer :: Nn, Ne
@@ -150,12 +176,13 @@ real(dp), allocatable :: nodes(:, :)
 integer, allocatable :: elems(:, :) ! elems(:, i) are nodes of the i-th element
 integer :: Nq, Nb
 real(dp), allocatable :: xin(:), xiq(:), wtq(:), A(:, :), rhs(:), sol(:), &
-        fullsol(:), solq(:, :, :), wtq2(:, :), phihq(:, :), dphihq(:, :)
-integer, allocatable :: in(:, :, :), ib(:, :, :)
-integer :: i, j
-integer, intent(in) :: Nex, Ney
+        fullsol(:), solq(:, :, :, :), wtq3(:, :, :), phihq(:, :), dphihq(:, :)
+integer, allocatable :: in(:, :, :, :), ib(:, :, :, :)
+integer :: i, j, k
+integer, intent(in) :: Nex, Ney, Nez
 
-call cartesian_mesh_2d(Nex, Ney, [0.0_dp, 0._dp], [1._dp, 1._dp], nodes, elems)
+call cartesian_mesh_3d(Nex, Ney, Nez, &
+    [0._dp, 0._dp, 0._dp], [1._dp, 1._dp, 1._dp], nodes, elems)
 Nn = size(nodes, 2)
 Ne = size(elems, 2)
 Nq = p+1
@@ -166,26 +193,26 @@ print *, "Nq =", Nq
 print *, "p =", p
 allocate(xin(p+1))
 call get_parent_nodes(2, p, xin)
-allocate(xiq(Nq), wtq(Nq), wtq2(Nq, Nq))
+allocate(xiq(Nq), wtq(Nq), wtq3(Nq, Nq, Nq))
 call get_parent_quad_pts_wts(1, Nq, xiq, wtq)
-forall(i=1:Nq, j=1:Nq) wtq2(i, j) = wtq(i)*wtq(j)
+forall(i=1:Nq, j=1:Nq, k=1:Nq) wtq3(i, j, k) = wtq(i)*wtq(j)*wtq(k)
 allocate(phihq(size(xiq), size(xin)))
 allocate(dphihq(size(xiq), size(xin)))
 ! Tabulate parent basis at quadrature points
 forall(i=1:size(xiq), j=1:size(xin))  phihq(i, j) =  phih(xin, j, xiq(i))
 forall(i=1:size(xiq), j=1:size(xin)) dphihq(i, j) = dphih(xin, j, xiq(i))
 
-call define_connect_tensor_2d(Nex, Ney, p, 1, in)
-call define_connect_tensor_2d(Nex, Ney, p, 2, ib)
+call define_connect_tensor_3d(Nex, Ney, Nez, p, 1, in)
+call define_connect_tensor_3d(Nex, Ney, Nez, p, 2, ib)
 Nb = maxval(ib)
 print *, "DOFs =", Nb
-allocate(A(Nb, Nb), rhs(Nb), sol(Nb), fullsol(maxval(in)), solq(Nq, Nq, Ne))
+allocate(A(Nb, Nb), rhs(Nb), sol(Nb), fullsol(maxval(in)), solq(Nq, Nq, Nq, Ne))
 
-call assemble_2d(xin, nodes, elems, ib, xiq, wtq2, phihq, dphihq, A, rhs)
+call assemble_3d(xin, nodes, elems, ib, xiq, wtq3, phihq, dphihq, A, rhs)
 sol = solve(A, rhs)
-call c2fullc_2d(in, ib, sol, fullsol)
-call fe2quad_2d(elems, xin, xiq, phihq, in, fullsol, solq)
-error = sol_error(nodes, elems, xiq, wtq2, solq)
+call c2fullc_3d(in, ib, sol, fullsol)
+call fe2quad_3d(elems, xin, xiq, phihq, in, fullsol, solq)
+error = sol_error(nodes, elems, xiq, wtq3, solq)
 
 end function
 
@@ -201,6 +228,6 @@ use poisson3d_code, only: solve_poisson
 
 real(dp) :: error
 
-error = solve_poisson(1, 1, 8)
+error = solve_poisson(1, 1, 1, 8)
 print *, "L2 error:", error
 end program
