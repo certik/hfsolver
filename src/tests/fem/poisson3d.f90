db578cb8d45478bae3b8f7fdbd6ec19a40b668d1
Ondřej Čertík <ondrej.certik@gmail.com>
2013-04-23 23:11:44 -0600

    use get_rhs

--- src/tests/fem/poisson3d.f90
+++ src/tests/fem/poisson3d.f90
@@ -5,7 +5,7 @@ use utils, only: assert, stop_error
 use constants, only: pi
 implicit none
 private
-public assemble_3d, sol_error, integral
+public assemble_3d, sol_error, integral, get_rhs
 
 contains
 
@@ -52,10 +52,11 @@ do e = 1, Ne
 end do
 end function
 
-subroutine assemble_3d(xin, nodes, elems, ib, xiq, wtq, phihq, dphihq, Am, rhs)
+subroutine assemble_3d(xin, nodes, elems, ib, xiq, wtq, phihq, dphihq, &
+        rhsq, Am, rhs)
 ! Assemble on a 3D hexahedral uniform mesh
 real(dp), intent(in):: xin(:), nodes(:, :), xiq(:), wtq(:, :, :), &
-    phihq(:, :), dphihq(:, :)
+    phihq(:, :), dphihq(:, :), rhsq(:, :, :, :)
 integer, intent(in):: elems(:, :), ib(:, :, :, :)
 real(dp), intent(out):: Am(:,:), rhs(:)
 integer :: Ne, p, e, i, j, iqx, iqy, iqz
@@ -109,13 +110,7 @@ do e = 1, Ne
     x = xp + nodes(1, elems(1, e))
     y = yp + nodes(2, elems(1, e))
     z = zp + nodes(3, elems(1, e))
-    do iqz = 1, size(xiq)
-    do iqy = 1, size(xiq)
-    do iqx = 1, size(xiq)
-        fq(iqx, iqy, iqz) = f(x(iqx), y(iqy), z(iqz))
-    end do
-    end do
-    end do
+    fq = rhsq(:, :, :, e)
     fq = fq * jac_det * wtq
     do bz = 1, p+1
     do by = 1, p+1
@@ -212,7 +207,7 @@ use types, only: dp
 use feutils, only: phih, dphih
 use fe_mesh, only: cartesian_mesh_3d, define_connect_tensor_3d, &
     c2fullc_3d, fe2quad_3d
-use poisson3d_assembly, only: assemble_3d, sol_error, integral
+use poisson3d_assembly, only: assemble_3d, sol_error, integral, get_rhs
 use feutils, only: get_parent_nodes, get_parent_quad_pts_wts
 use linalg, only: solve
 use constants, only: pi
@@ -229,7 +224,8 @@ real(dp), allocatable :: nodes(:, :)
 integer, allocatable :: elems(:, :) ! elems(:, i) are nodes of the i-th element
 integer :: Nq, Nb
 real(dp), allocatable :: xin(:), xiq(:), wtq(:), A(:, :), rhs(:), sol(:), &
-        fullsol(:), solq(:, :, :, :), wtq3(:, :, :), phihq(:, :), dphihq(:, :)
+        fullsol(:), solq(:, :, :, :), wtq3(:, :, :), phihq(:, :), dphihq(:, :),&
+        rhsq(:, :, :, :)
 integer, allocatable :: in(:, :, :, :), ib(:, :, :, :)
 integer :: i, j, k
 integer, intent(in) :: Nex, Ney, Nez
@@ -260,8 +256,10 @@ call define_connect_tensor_3d(Nex, Ney, Nez, p, 2, ib)
 Nb = maxval(ib)
 print *, "DOFs =", Nb
 allocate(A(Nb, Nb), rhs(Nb), sol(Nb), fullsol(maxval(in)), solq(Nq, Nq, Nq, Ne))
+allocate(rhsq(Nq, Nq, Nq, Ne))
 
-call assemble_3d(xin, nodes, elems, ib, xiq, wtq3, phihq, dphihq, A, rhs)
+rhsq = get_rhs(nodes, elems, xiq)
+call assemble_3d(xin, nodes, elems, ib, xiq, wtq3, phihq, dphihq, rhsq, A, rhs)
 sol = solve(A, rhs)
 call c2fullc_3d(in, ib, sol, fullsol)
 call fe2quad_3d(elems, xin, xiq, phihq, in, fullsol, solq)
