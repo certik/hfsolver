bd9e17c4a31c95154e0991a1cf9d59bb576f6d94
Ondřej Čertík <ondrej.certik@gmail.com>
2013-06-19 21:06:03 -0600

    Implement brent()

--- src/optimize.f90
+++ src/optimize.f90
@@ -44,4 +44,109 @@ end do
 c = (a_ + b_)/2
 end function
 
+subroutine brent(f, xa, xb, xc, tol, maxiter, xmin, fxmin)
+procedure(func) :: f
+real(dp), intent(in) :: xa, xb, xc, tol
+integer, intent(in) :: maxiter
+real(dp), intent(out) :: xmin, fxmin
+real(dp), parameter :: mintol = 1e-11_dp, golden_ratio = (1+sqrt(5._dp))/2, &
+        cg = 1 - golden_ratio
+real(dp) :: tol1, tol2, tmp1, tmp2, deltax, dx_temp, rat, xmid, &
+        a, b, p, u, v, w, x, fu, fv, fw, fx
+integer :: iter
+x = xb; w = xb; v = xb
+fx = f(x)
+fw = fx; fv = fx
+if (xa < xc) then
+    a = xa
+    b = xc
+else
+    a = xc
+    b = xa
+end if
+deltax = 0
+do iter = 1, maxiter
+    tol1 = tol*abs(x) + mintol
+    tol2 = 2*tol1
+    xmid = 0.5_dp*(a + b)
+    if (abs(x - xmid) < (tol2 - 0.5_dp*(b - a))) then ! check for convergence
+        xmin = x
+        fxmin = fx
+        return
+    end if
+    if (abs(deltax) <= tol1) then
+        if (x >= xmid) then           ! do a golden section step
+            deltax = a - x
+        else
+            deltax = b - x
+        end if
+        rat = cg*deltax
+    else
+        tmp1 = (x - w)*(fx - fv)      ! do a parabolic step
+        tmp2 = (x - v)*(fx - fw)
+        p = (x - v)*tmp2 - (x - w)*tmp1;
+        tmp2 = 2*(tmp2 - tmp1)
+        if (tmp2 > 0) then
+            p = -p
+        end if
+        tmp2 = abs(tmp2)
+        dx_temp = deltax
+        deltax = rat
+        if ((p > tmp2*(a - x)) .and. (p < tmp2*(b - x)) .and. &
+                (abs(p) < abs(0.5_dp*tmp2*dx_temp))) then ! check parabolic fit
+            rat = p*1.0 / tmp2        ! if parabolic step is useful.
+            u = x + rat
+            if ((u - a) < tol2 .or. (b - u) < tol2) then
+                if (xmid - x >= 0) then
+                    rat = tol1
+                else
+                    rat = -tol1
+                end if
+            end if
+        else
+            if (x >= xmid) then       ! if it's not do a golden section step
+                deltax = a - x
+            else
+                deltax = b - x
+            end if
+            rat = cg*deltax
+        end if
+    end if
+
+    if (abs(rat) < tol1) then         ! update by at least tol1
+        if (rat >= 0) then
+            u = x + tol1
+        else
+            u = x - tol1
+        end if
+    else
+        u = x + rat
+    end if
+    fu = f(u) ! calculate new output value
+
+    if (fu > fx) then                 ! if it's bigger than current
+        if (u < x) then
+            a = u
+        else
+            b = u
+        end if
+        if ((fu <= fw) .or. (w == x)) then
+            v = w; w = u; fv = fw; fw = fu
+        else if ((fu <= fv) .or. (v == x) .or. (v == w)) then
+            v = u; fv = fu
+        end if
+    else
+        if (u >= x) then
+            a = x
+        else
+            b = x
+        end if
+        v = w; w = x; x = u
+        fv = fw; fw = fx; fx = fu
+    end if
+end do
+call stop_error("brent: The maximum number of iterations exceeded.")
+end subroutine
+
+
 end module
