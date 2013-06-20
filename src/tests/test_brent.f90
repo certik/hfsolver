e7da0cceeb92b0fdd558d39e62f1b34f0b532e5d
Ondřej Čertík <ondrej.certik@gmail.com>
2013-06-19 21:58:40 -0600

    One more test

--- src/tests/test_brent.f90
+++ src/tests/test_brent.f90
@@ -14,6 +14,8 @@ call assert(abs(xmin - 0) < eps)
 
 call brent(f2, -4._dp, 0._dp, 4._dp, eps, 30, xmin, fxmin)
 call assert(abs(xmin - 0.865474033101_dp) < eps)
+call brent(f2, -4._dp, 1._dp, 4._dp, eps, 30, xmin, fxmin)
+call assert(abs(xmin - 0.865474033101_dp) < eps)
 
 contains
 
