7282adb565585a2760a1ca3503370b76391db620
Ondřej Čertík <ondrej.certik@gmail.com>
2013-07-20 17:02:43 -0600

    brent: make tests more robust

--- src/tests/test_brent.f90
+++ src/tests/test_brent.f90
@@ -13,9 +13,9 @@ call brent(f1, -1._dp, 0._dp, 1._dp, eps, 20, xmin, fxmin)
 call assert(abs(xmin - 0) < eps)
 
 call brent(f2, -4._dp, 0._dp, 4._dp, eps, 30, xmin, fxmin)
-call assert(abs(xmin - 0.865474033101_dp) < eps)
+call assert(abs(xmin - 0.865474033102_dp) < eps)
 call brent(f2, -4._dp, 1._dp, 4._dp, eps, 30, xmin, fxmin)
-call assert(abs(xmin - 0.865474033101_dp) < eps)
+call assert(abs(xmin - 0.865474033102_dp) < eps)
 
 xa = -4
 xb = -3
