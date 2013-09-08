e6104bfb354487dba8202740c34d4c7367da38d2
Ondřej Čertík <ondrej.certik@gmail.com>
2013-09-08 14:04:43 -0700

    Implement simple fft
    And test it.

--- src/fourier.f90
+++ src/fourier.f90
@@ -5,9 +5,10 @@ module fourier
 
 use types, only: dp
 use constants, only: i_, pi
+use utils, only: stop_error
 implicit none
 private
-public dft, idft
+public dft, idft, fft
 
 contains
 
@@ -35,6 +36,24 @@ N = size(p)
 forall(i=0:N-1, j=0:N-1, i >= j) F(i+1, j+1) = exp(+2*pi*i_*i*j/ N)
 forall(i=1:N, j=1:N, i < j) F(i, j) = F(j, i)
 x = matmul(F, p) / N
+end function
+
+function fft(x) result(p)
+! A recursive implementation of the 1D Cooley-Tukey FFT
+real(dp), intent(in) :: x(:)
+complex(dp) :: p(size(x)), X_even(size(x)/2), X_odd(size(x)/2)
+complex(dp) :: factor(size(x))
+integer :: N, i
+N = size(x)
+if (iand(N, N-1) /= 0) call stop_error("size of x must be a power of 2")
+if (N <= 4) then
+    p = dft(x)
+else
+    X_even = fft(x(::2))
+    X_odd = fft(x(2::2))
+    forall(i=0:N-1) factor(i+1) = exp(-2*pi*i_*i/N)
+    p = [X_even + factor(:N / 2) * X_odd, X_even + factor(N / 2+1:) * X_odd]
+end if
 end function
 
 end module
--- src/tests/test_fourier.f90
+++ src/tests/test_fourier.f90
@@ -1,6 +1,6 @@
 program test_fourier
 use types, only: dp
-use fourier, only: dft, idft
+use fourier, only: dft, idft, fft
 use utils, only: assert
 use constants, only: i_
 implicit none
@@ -18,12 +18,55 @@ allocate(x(4), xdft(4))
 x = [1, 2, 3, 4]
 xdft = [10 + 0*i_, -2+2*i_, -2+0*i_, -2-2*i_]
 call assert(all(abs(dft(x) - xdft) < 1e-12_dp))
+call assert(all(abs(fft(x) - xdft) < 1e-12_dp))
 call assert(all(abs(idft(xdft) - x) < 1e-12_dp))
 deallocate(x, xdft)
 
 allocate(x(5))
 x = [1, 2, 3, 4, 5]
 call assert(all(abs(idft(dft(x)) - x) < 1e-12_dp))
+deallocate(x)
+
+n = 8
+allocate(x(n))
+forall(i = 1:n) x(i) = i
+call assert(all(abs(idft(dft(x)) - x) < 1e-10_dp))
+call assert(all(abs(idft(fft(x)) - x) < 1e-10_dp))
+deallocate(x)
+
+n = 16
+allocate(x(n))
+forall(i = 1:n) x(i) = i
+call assert(all(abs(idft(dft(x)) - x) < 1e-10_dp))
+call assert(all(abs(idft(fft(x)) - x) < 1e-10_dp))
+deallocate(x)
+
+n = 32
+allocate(x(n))
+forall(i = 1:n) x(i) = i
+call assert(all(abs(idft(dft(x)) - x) < 1e-10_dp))
+call assert(all(abs(idft(fft(x)) - x) < 1e-10_dp))
+deallocate(x)
+
+n = 64
+allocate(x(n))
+forall(i = 1:n) x(i) = i
+call assert(all(abs(idft(dft(x)) - x) < 1e-10_dp))
+call assert(all(abs(idft(fft(x)) - x) < 1e-10_dp))
+deallocate(x)
+
+n = 128
+allocate(x(n))
+forall(i = 1:n) x(i) = i
+call assert(all(abs(idft(dft(x)) - x) < 1e-10_dp))
+call assert(all(abs(idft(fft(x)) - x) < 1e-10_dp))
+deallocate(x)
+
+n = 256
+allocate(x(n))
+forall(i = 1:n) x(i) = i
+call assert(all(abs(idft(dft(x)) - x) < 1e-10_dp))
+call assert(all(abs(idft(fft(x)) - x) < 1e-10_dp))
 deallocate(x)
 
 n = 400
