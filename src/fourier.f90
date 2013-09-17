e569fddf2eb1a475b1b8ce78a585604ea3fdca0e
Ondřej Čertík <ondrej.certik@gmail.com>
2013-09-17 16:39:08 -0600

    Add PASSF4_f77

--- src/fourier.f90
+++ src/fourier.f90
@@ -148,6 +148,65 @@ do K = 1, L1
 end do
 end subroutine
 
+      SUBROUTINE PASSF4_f77(IDO,L1,CC,CH,WA1,WA2,WA3)
+      integer, intent(in) :: IDO, L1
+      real(dp), intent(in) :: CC(IDO,4,L1), WA1(:), WA2(:), WA3(:)
+      real(dp), intent(out) :: CH(IDO,L1,4)
+      real(dp) :: CR2, CR3, CR4
+      real(dp) :: CI2, CI3, CI4
+      real(dp) :: TR1, TR2, TR3, TR4
+      real(dp) :: TI1, TI2, TI3, TI4
+      integer :: I, K
+      IF (IDO .NE. 2) GO TO 102
+      DO 101 K=1,L1
+         TI1 = CC(2,1,K)-CC(2,3,K)
+         TI2 = CC(2,1,K)+CC(2,3,K)
+         TR4 = CC(2,2,K)-CC(2,4,K)
+         TI3 = CC(2,2,K)+CC(2,4,K)
+         TR1 = CC(1,1,K)-CC(1,3,K)
+         TR2 = CC(1,1,K)+CC(1,3,K)
+         TI4 = CC(1,4,K)-CC(1,2,K)
+         TR3 = CC(1,2,K)+CC(1,4,K)
+         CH(1,K,1) = TR2+TR3
+         CH(1,K,3) = TR2-TR3
+         CH(2,K,1) = TI2+TI3
+         CH(2,K,3) = TI2-TI3
+         CH(1,K,2) = TR1+TR4
+         CH(1,K,4) = TR1-TR4
+         CH(2,K,2) = TI1+TI4
+         CH(2,K,4) = TI1-TI4
+  101 CONTINUE
+      RETURN
+  102 DO 104 K=1,L1
+         DO 103 I=2,IDO,2
+            TI1 = CC(I,1,K)-CC(I,3,K)
+            TI2 = CC(I,1,K)+CC(I,3,K)
+            TI3 = CC(I,2,K)+CC(I,4,K)
+            TR4 = CC(I,2,K)-CC(I,4,K)
+            TR1 = CC(I-1,1,K)-CC(I-1,3,K)
+            TR2 = CC(I-1,1,K)+CC(I-1,3,K)
+            TI4 = CC(I-1,4,K)-CC(I-1,2,K)
+            TR3 = CC(I-1,2,K)+CC(I-1,4,K)
+            CH(I-1,K,1) = TR2+TR3
+            CR3 = TR2-TR3
+            CH(I,K,1) = TI2+TI3
+            CI3 = TI2-TI3
+            CR2 = TR1+TR4
+            CR4 = TR1-TR4
+            CI2 = TI1+TI4
+            CI4 = TI1-TI4
+            CH(I-1,K,2) = WA1(I-1)*CR2+WA1(I)*CI2
+            CH(I,K,2) = WA1(I-1)*CI2-WA1(I)*CR2
+            CH(I-1,K,3) = WA2(I-1)*CR3+WA2(I)*CI3
+            CH(I,K,3) = WA2(I-1)*CI3-WA2(I)*CR3
+            CH(I-1,K,4) = WA3(I-1)*CR4+WA3(I)*CI4
+            CH(I,K,4) = WA3(I-1)*CI4-WA3(I)*CR4
+  103    CONTINUE
+  104 CONTINUE
+      RETURN
+      END
+
+
 subroutine fft_pass_inplace(x)
 complex(dp), intent(inout) :: x(:)
 complex(dp), dimension(size(x)) :: angles, CH
