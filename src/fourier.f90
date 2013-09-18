d78b99d1f88fc5003920ef3fdf4afa6644876289
Ondřej Čertík <ondrej.certik@gmail.com>
2013-09-17 21:39:36 -0600

    Implement CFFTF1

--- src/fourier.f90
+++ src/fourier.f90
@@ -337,6 +337,75 @@ if (.not. CH_is_result) then
 end if
 end subroutine
 
+
+      SUBROUTINE CFFTF1 (N,C,CH,WA,IFAC)
+      ! NOTE: PASSF3, PASSF5 and PASSF are commented out for now
+      integer, intent(in) :: N
+      complex(dp), intent(inout) :: C(:)
+      complex(dp), intent(out) :: CH(:)
+      complex(dp), intent(in) :: WA(:)
+      integer, intent(in) :: IFAC(:)
+      integer :: k1, l1, na, iw, ip, l2, ido, idot, idl1, ix2, ix3, i, ix4, &
+          n2
+      NA = 0
+      L1 = 1
+      IW = 1
+      DO 116 K1=1,size(ifac)
+         IP = IFAC(K1)
+         L2 = IP*L1
+         IDO = N/L2
+         IDOT = IDO+IDO
+         IDL1 = IDOT*L1
+         IF (IP .NE. 4) GO TO 103
+         IX2 = IW+IDOT
+         IX3 = IX2+IDOT
+         IF (NA .NE. 0) GO TO 101
+         call passf4(IDOT,L1,C,CH,WA(IW/2:),WA(IX2/2:),WA(IX3/2:))
+         GO TO 102
+  101    call passf4(IDOT,L1,CH,C,WA(IW/2:),WA(IX2/2:),WA(IX3/2:))
+  102    NA = 1-NA
+         GO TO 115
+  103    IF (IP .NE. 2) GO TO 106
+         IF (NA .NE. 0) GO TO 104
+         call passf2(IDOT,L1,C,CH,WA(IW/2:))
+         GO TO 105
+  104    call passf2(IDOT,L1,CH,C,WA(IW/2:))
+  105    NA = 1-NA
+         GO TO 115
+  106    IF (IP .NE. 3) GO TO 109
+         IX2 = IW+IDOT
+         IF (NA .NE. 0) GO TO 107
+         !CALL PASSF3 (IDOT,L1,C,CH,WA(IW),WA(IX2))
+         GO TO 108
+  107    continue !CALL PASSF3 (IDOT,L1,CH,C,WA(IW),WA(IX2))
+  108    NA = 1-NA
+         GO TO 115
+  109    IF (IP .NE. 5) GO TO 112
+         IX2 = IW+IDOT
+         IX3 = IX2+IDOT
+         IX4 = IX3+IDOT
+         IF (NA .NE. 0) GO TO 110
+         !CALL PASSF5 (IDOT,L1,C,CH,WA(IW),WA(IX2),WA(IX3),WA(IX4))
+         GO TO 111
+  110    continue !CALL PASSF5 (IDOT,L1,CH,C,WA(IW),WA(IX2),WA(IX3),WA(IX4))
+  111    NA = 1-NA
+         GO TO 115
+  112    IF (NA .NE. 0) GO TO 113
+         !CALL PASSF (NAC,IDOT,IP,L1,IDL1,C,C,C,CH,CH,WA(IW))
+         GO TO 114
+  113    continue !CALL PASSF (NAC,IDOT,IP,L1,IDL1,CH,CH,CH,C,C,WA(IW))
+  114    continue !IF (NAC .NE. 0) NA = 1-NA
+  115    L1 = L2
+         IW = IW+(IP-1)*IDOT
+  116 CONTINUE
+      IF (NA .EQ. 0) RETURN
+      N2 = N+N
+      DO 117 I=1,N2
+         C(I) = CH(I)
+  117 CONTINUE
+      RETURN
+      END
+
 function fft_pass(x) result(p)
 real(dp), intent(in) :: x(:)
 complex(dp) :: p(size(x))
