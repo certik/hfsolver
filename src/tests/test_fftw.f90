1d36652bb8f2a4e97d7c43cc6f65d0716739768e
Ondřej Čertík <ondrej.certik@gmail.com>
2013-09-23 14:49:42 -0600

    Just keep the fast versions

--- src/tests/test_fftw.f90
+++ src/tests/test_fftw.f90
@@ -9,7 +9,6 @@ implicit none
 
 type(c_ptr) :: plan
 complex(dp), dimension(4, 8, 16) :: y3, ydft3
-complex(dp), allocatable :: x(:), xdft(:), x3(:, :, :), xdft3(:, :, :)
 integer :: n
 complex(dp), pointer :: px(:), pxdft(:), px3(:, :, :), pxdft3(:, :, :)
 type(c_ptr) :: p1, p2
@@ -22,20 +21,6 @@ call fftw_execute_dft(plan, y3, ydft3)
 call fftw_destroy_plan(plan)
 
 n = 1024**2
-! This works, but is slow due to x,xdft not being aligned to exploit SIMD
-print *, "1D FFT of size n=", n, "with Fortran allocation"
-allocate(x(n), xdft(n))
-call cpu_time(t1)
-!plan = fftw_plan_dft_1d(n, x, xdft, FFTW_FORWARD, FFTW_ESTIMATE)
-plan = fftw_plan_dft_1d(n, x, xdft, FFTW_FORWARD, FFTW_MEASURE)
-call cpu_time(t2)
-call fftw_execute_dft(plan, x, xdft)
-call cpu_time(t3)
-print *, "Total time:", (t3-t1)*1000, "ms"
-print *, "init:      ", (t2-t1)*1000, "ms"
-print *, "calc:      ", (t3-t2)*1000, "ms"
-call fftw_destroy_plan(plan)
-deallocate(x, xdft)
 
 ! This is fast
 print *, "1D FFT of size n=", n, "with FFTW allocation"
@@ -57,19 +42,6 @@ call fftw_free(p1)
 call fftw_free(p2)
 
 n = 256
-! This works, but is slow due to x,xdft not being aligned to exploit SIMD
-print *, "3D FFT of size n=", n, "^3  with Fortran allocation"
-allocate(x3(n, n, n), xdft3(n, n, n))
-call cpu_time(t1)
-plan = fftw_plan_dft_3d(n, n, n, x3, xdft3, FFTW_FORWARD, FFTW_MEASURE)
-call cpu_time(t2)
-call fftw_execute_dft(plan, x3, xdft3)
-call cpu_time(t3)
-print *, "Total time:", (t3-t1)*1000, "ms"
-print *, "init:      ", (t2-t1)*1000, "ms"
-print *, "calc:      ", (t3-t2)*1000, "ms"
-call fftw_destroy_plan(plan)
-deallocate(x3, xdft3)
 
 ! This is fast
 print *, "1D FFT of size n=", n, "^3  with FFTW allocation"
