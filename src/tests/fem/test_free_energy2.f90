7a6c00302bfd96b44a4e2c31c5dfa950b904749a
Ondřej Čertík <ondrej.certik@gmail.com>
2013-11-21 23:40:17 -0700

    Remove unnecessary lines

--- src/tests/fem/test_free_energy2.f90
+++ src/tests/fem/test_free_energy2.f90
@@ -500,7 +500,7 @@ use splines, only: spline3pars, iixmin, poly3, spline3ders
 use interp3d, only: trilinear
 implicit none
 real(dp) :: Eh, Een, Ts, Exc, Etot
-integer :: p, DOF, u
+integer :: p, DOF
 real(dp) :: Z, Ediff
 real(dp), allocatable :: R(:), V(:), c(:, :)
 real(dp), allocatable :: tmp(:), Vd(:), Vdd(:), density_en(:)
@@ -510,14 +510,6 @@ call read_pseudo("H.pseudo.gaussian", R, V, Z, Ediff)
 allocate(tmp(size(R)), Vd(size(R)), Vdd(size(R)), density_en(size(R)))
 call spline3ders(R, V, R, tmp, Vd, Vdd)
 density_en = -(Vdd+2*Vd/R)/(4*pi)
-open(newunit=u, file="H.pseudo.density", status="replace")
-write(u, "(a)") "# Pseudopotential. The lines are: r, V(r), V'(r), V''(r), n(r)"
-write(u, *) R
-write(u, *) V
-write(u, *) Vd
-write(u, *) Vdd
-write(u, *) density_en
-close(u)
 allocate(c(0:4, size(R, 1)-1))
 call spline3pars(R, density_en, [2, 2], [0._dp, 0._dp], c)
 Rcut = R(size(R))
