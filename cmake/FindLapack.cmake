c83a7aef515e6738f7e41adbe4b99a0f8a3383b3
Ondřej Čertík <ondrej.certik@gmail.com>
2014-02-13 22:32:13 -0700

    Find proper lapack

--- cmake/FindLapack.cmake
+++ cmake/FindLapack.cmake
@@ -1,4 +1,8 @@
-set(LAPACK_LIBRARIES lapack blas)
+find_library(LAPACK_LIB lapack $ENV{PYTHONHPC}/lib NO_DEFAULT_PATH)
+find_library(BLAS_LIB blas $ENV{PYTHONHPC}/lib NO_DEFAULT_PATH)
+set(LAPACK_LIBRARIES ${LAPACK_LIB} ${BLAS_LIB})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Lapack DEFAULT_MSG LAPACK_LIBRARIES)
+
+mark_as_advanced(LAPACK_LIB BLAS_LIB)
