1aadbee08ce5642d3a06c84f1b32f6759ce3a3c5
Ondřej Čertík <ondrej.certik@gmail.com>
2014-06-04 22:42:43 -0600

    Test ewald() for NaCl using FCC lattice
    It gives exactly the same results as before.

--- src/tests/test_ewald.f90
+++ src/tests/test_ewald.f90
@@ -10,20 +10,24 @@ use ewald_sums, only: ewald
 use utils, only: assert, init_random
 implicit none
 
-integer, parameter :: natom=8, ntypat=2
+integer :: natom, ntypat
 ! Various NaCl lattice constants in A
 real(dp), parameter :: Llist(*) = [5.6402_dp, 5.5_dp, 4.5_dp, 6.5_dp, 10._dp]
 real(dp) :: ucvol
-integer :: typat(natom)
-real(dp) :: gmet(3, 3), rmet(3, 3), xred(3, natom), zion(ntypat)
+integer, allocatable :: typat(:)
+real(dp) :: gmet(3, 3), rmet(3, 3)
 real(dp) :: eew
-real(dp) :: grewtn(3, natom)
+real(dp), allocatable :: xred(:, :), zion(:), grewtn(:, :)
 
 real(dp) :: L, alpha, E_ewald, E_madelung
 integer :: i
 
 alpha = 1.74756459463318219064_dp ! Madelung constant for NaCl
 
+! Conventional cell:
+natom = 8
+ntypat = 2
+allocate(xred(3, natom), zion(ntypat), grewtn(3, natom), typat(natom))
 ! Cl^-
 xred(:, 1) = [0._dp, 0._dp, 0._dp]
 xred(:, 2) = [1._dp/2, 1._dp/2, 0._dp]
@@ -60,5 +64,44 @@ do i = 1, size(Llist)
     print *, "error:   ", abs(E_ewald - E_madelung), "a.u."
     call assert(abs(E_ewald - E_madelung) < 1e-14_dp)
 end do
+deallocate(xred, zion, grewtn, typat)
+
+print *, "--------"
+
+! Primitive cell (FCC lattice)
+natom = 2
+ntypat = 2
+allocate(xred(3, natom), zion(ntypat), grewtn(3, natom), typat(natom))
+! Cl^-
+xred(:, 1) = [0._dp, 0._dp, 0._dp]
+! Na^+
+xred(:, 2) = [1._dp/2, 1._dp/2, 1._dp/2]
+typat = [1, 2]
+zion = [-1._dp, +1._dp]
+
+do i = 1, size(Llist)
+    L = Llist(i) * ang2bohr
+    ucvol = L**3/4
+
+    rmet(1, :) = [1._dp/2, 1._dp/4, 1._dp/4]
+    rmet(2, :) = [1._dp/4, 1._dp/2, 1._dp/4]
+    rmet(3, :) = [1._dp/4, 1._dp/4, 1._dp/2]
+    rmet = rmet * L**2
+    gmet(1, :) = [3._dp, -1._dp, -1._dp]
+    gmet(2, :) = [-1._dp, 3._dp, -1._dp]
+    gmet(3, :) = [-1._dp, -1._dp, 3._dp]
+    gmet = gmet / L**2
+
+    call ewald(eew,gmet,grewtn,natom,ntypat,rmet,typat,ucvol,xred,zion)
+
+    E_ewald = eew / (natom/ntypat)
+    E_madelung = -2*alpha/L
+    print *, "a =", L/ang2bohr*100, "pm"
+    print *, "Ewald:   ", E_ewald / kJmol2Ha, "kJ/mol"
+    print *, "Madelung:", E_madelung / kJmol2Ha, "kJ/mol"
+    print *, "error:   ", abs(E_ewald - E_madelung), "a.u."
+    call assert(abs(E_ewald - E_madelung) < 1e-14_dp)
+end do
+deallocate(xred, zion, grewtn, typat)
 
 end program
