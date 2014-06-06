a9c281827aa3e991ddb0b7a4852518f6ffe39865
Ondřej Čertík <ondrej.certik@gmail.com>
2014-06-05 18:05:04 -0600

    Read OFMD.input, use FCC

--- src/tests/test_ofmd_fft.f90
+++ src/tests/test_ofmd_fft.f90
@@ -1,8 +1,8 @@
 program test_ofmd_fft
 use types, only: dp
-use constants, only: ang2bohr, K2au, density2gcm3, u2au, s2au
-use md, only: velocity_verlet, minimize_energy, random_positions, &
-                calc_min_distance
+use constants, only: ang2bohr, K2au, density2gcm3, u2au, s2au, Ha2eV
+use md, only: velocity_verlet, minimize_energy, positions_random, &
+                calc_min_distance, positions_fcc
 use ewald_sums, only: ewald
 use random, only: randn
 use utils, only: init_random, stop_error
@@ -10,40 +10,38 @@ implicit none
 
 ! All variables are in Hartree atomic units
 
-integer, parameter :: N = 4
+integer :: N = 4
 integer :: i, steps, u
-real(dp) :: dt, V(3, N), X(3, N), f(3, N), m(N), L, t, Rcut, eps, sigma, rho
+real(dp) :: dt, L, t, Rcut, eps, sigma, rho
+real(dp), allocatable :: V(:, :), X(:, :), f(:, :), m(:)
 real(dp) :: Temp, Ekin, Epot, Temp_current, t3, t4
+integer :: dynamics, functional, Ng, Nspecies, start
 
+call read_input("OFMD.input", Temp, rho, Nspecies, N, start, dynamics, &
+            functional, Ng, steps, dt)
+allocate(X(3, N), V(3, N), f(3, N), m(N))
 
-steps = 800
-dt = 1e-14_dp * s2au
-m = 39.948_dp * u2au ! Using Argon mass in atomic mass units [u]
-sigma = 3.4_dp * ang2bohr
-eps = 120 * K2au
-Rcut = 2.25_dp*sigma
-!rho = 1.374_dp / density2gcm3
-rho = 0.4_dp / density2gcm3
-Temp = 94.4_dp * K2au
-
+m = 1._dp * u2au ! Using Hydrogen mass in atomic mass units [u]
 L = (sum(m) / rho)**(1._dp/3)
-rho = sum(m) / L**3
 print *, "Input:"
 print *, "N =", N
-print *, "rho = ", rho * density2gcm3, "g/cm^3 = ", rho, "a.u."
-print *, "T =", Temp / K2au, "K =", Temp, "a.u."
-print "('dt =', es10.2, ' s = ', es10.2, ' ps = ', f8.2, ' a.u.')", dt/s2au, &
-    dt/s2au * 1e12_dp, dt
-print *, "eps =", eps / K2au, "K/kB =", eps, "a.u.";
-print *, "sigma =", sigma / ang2bohr, " A =", sigma, "a.u."
-print *, "Rcut =", Rcut / sigma, " sigma =", Rcut, "a.u."
+print *, "rho = ",  rho, "a.u. =", rho * density2gcm3, "g/cm^3"
+print *, "T =", Temp, "a.u. =", Temp / K2au, "K =", Temp * Ha2eV, "eV"
+print "('dt =', f8.2, ' a.u. = ', es10.2, ' s = ', es10.2, ' ps')", dt, &
+    dt/s2au, dt/s2au * 1e12_dp
 print *
 print *, "Calculated quantities:"
-print *, "L =", L/sigma, "sigma = ", L, "a.u."
+print *, "L =", L, "a.u."
 print *
 
 call init_random()
-call random_positions(X, L, 2**(1._dp/6)*sigma, 10)
+!call positions_random(X, L, 2**(1._dp/6)*sigma, 10)
+call positions_fcc(X, L)
+print *, "Positions:"
+do i = 1, N
+    print *, i, X(:, i)
+end do
+stop "OK"
 print *, "Distances:"
 do i = 2, N
     print *, i, calc_min_distance(X(:, :i-1), L, X(:, i))
@@ -157,5 +155,23 @@ contains
     real(dp), intent(in) :: m(:) ! masses
     Ekin = sum(m*sum(V**2, dim=1))/2
     end function
+
+    subroutine read_input(filename, T, density, Nspecies, N, start, dynamics, &
+            functional, Ng, steps, dt)
+    ! Reads the input file, returns values in a.u.
+    character(len=*), intent(in) :: filename
+    real(dp), intent(out) :: T, density, dt
+    integer, intent(out) :: Nspecies, N, start, dynamics, functional, Ng, steps
+    integer :: u
+    open(newunit=u, file=filename, status="old")
+    read(u, *) T, density, Nspecies, N
+    read(u, *) start
+    read(u, *) dynamics, functional, Ng
+    read(u, *) steps, dt
+    close(u)
+    ! Convert to atomic units:
+    T = T / Ha2eV
+    density = density / density2gcm3
+    end subroutine
 
 end program
