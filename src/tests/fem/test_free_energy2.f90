01b811196b4ba548d877fb2c6d561b3f57c2cf46
Ondřej Čertík <ondrej.certik@gmail.com>
2014-08-13 22:41:51 -0600

    Remove unused parts

--- src/tests/fem/test_free_energy2.f90
+++ src/tests/fem/test_free_energy2.f90
@@ -35,18 +35,16 @@ real(dp), allocatable :: nodes(:, :)
 integer, allocatable :: elems(:, :) ! elems(:, i) are nodes of the i-th element
 integer :: Nq
 real(dp), allocatable :: xin(:), xiq(:), wtq(:), &
-        wtq3(:, :, :), phihq(:, :), dphihq(:, :), free_energies(:)
+        wtq3(:, :, :), phihq(:, :), dphihq(:, :)
 real(dp), allocatable, dimension(:, :, :, :) :: nenq_pos, nq_pos, Hpsi, &
     psi, psi_, psi_prev, ksi, ksi_prev, phi, phi_prime, eta
 real(dp), allocatable, dimension(:, :, :, :, :, :) :: Am_loc, phi_v
 integer, allocatable :: in(:, :, :, :), ib(:, :, :, :)
-integer :: i, j, k, iter, max_iter
+integer :: i, j, k, max_iter
 integer, intent(in) :: Nex, Ney, Nez
-real(dp) :: Lx, Ly, Lz, mu, energy_eps, last3, brent_eps, free_energy_, &
-    gamma_d, gamma_n, theta, theta_a, theta_b, theta_c
+real(dp) :: Lx, Ly, Lz, energy_eps, brent_eps, free_energy_
 real(dp) :: Nelec, jac_det
 real(dp) :: psi_norm
-real(dp) :: fa, fb, fc
 
 energy_eps = 3.6749308286427368e-5_dp
 brent_eps = 1e-3_dp
@@ -113,19 +111,6 @@ print *, "Initial norm of psi:", psi_norm
 psi = sqrt(Nelec / psi_norm) * psi
 psi_norm = integral(nodes, elems, wtq3, psi**2)
 print *, "norm of psi:", psi_norm
-! This returns H[n] = delta F / delta n, we save it to the Hpsi variable to
-! save space:
-call free_energy_derivative(nodes, elems, in, ib, Nb, Lx, Ly, Lz, xin, &
-    xiq, wtq3, T_au, &
-    nenq_pos, psi**2, phihq, dphihq, Hpsi)
-! Hpsi = H[psi] = delta F / delta psi = 2*H[n]*psi, due to d/dpsi = 2 psi d/dn
-Hpsi = Hpsi * 2*psi
-mu = 1._dp / Nelec * integral(nodes, elems, wtq3, 0.5_dp * psi * Hpsi)
-ksi = 2*mu*psi - Hpsi
-phi = ksi
-phi_prime = phi - 1._dp / Nelec *  integral(nodes, elems, wtq3, phi * psi) * psi
-eta = sqrt(Nelec / integral(nodes, elems, wtq3, phi_prime**2)) * phi_prime
-theta = pi/2
 call free_energy(nodes, elems, in, ib, Nb, Lx, Ly, Lz, xin, xiq, wtq3, T_au, &
     nenq_pos, psi**2, phihq, Am_loc, phi_v, jac_det, &
     Eh, Een, Ts, Exc, free_energy_)
@@ -140,70 +125,6 @@ call assert(abs(Ts - 10.61904507_dp) < 1e-8_dp)
 call assert(abs(Een - (-3.77207363_dp)) < 3e-3_dp)
 call assert(abs(Eh - 1.30109486_dp) < 3e-4_dp)
 call assert(abs(Exc - (-1.43805889_dp)) < 1e-8_dp)
-stop "OK"
-allocate(free_energies(max_iter))
-gamma_n = 0
-do iter = 1, max_iter
-    theta_a = 0
-    theta_b = mod(theta, 2*pi)
-    call bracket(func, theta_a, theta_b, theta_c, fa, fb, fc, 100._dp, 20, verbose=.false.)
-    call brent(func, theta_a, theta_b, theta_c, brent_eps, 50, theta, &
-        free_energy_, verbose=.true.)
-    ! TODO: We probably don't need to recalculate free_energy_ here:
-    psi_prev = psi
-    psi = cos(theta) * psi + sin(theta) * eta
-    call free_energy(nodes, elems, in, ib, Nb, Lx, Ly, Lz, xin, xiq, wtq3, &
-        T_au, &
-        nenq_pos, psi**2, phihq, Am_loc, phi_v, jac_det, &
-        Eh, Een, Ts, Exc, free_energy_)
-    print *, "Iteration:", iter
-    psi_norm = integral(nodes, elems, wtq3, psi**2)
-    print *, "Norm of psi:", psi_norm
-    print *, "mu =", mu
-    print *, "|ksi| =", sqrt(gamma_n)
-    print *, "Summary of energies [a.u.]:"
-    print "('    Ts   = ', f14.8)", Ts
-    print "('    Een  = ', f14.8)", Een
-    print "('    Eee  = ', f14.8)", Eh
-    print "('    Exc  = ', f14.8)", Exc
-    print *, "   ---------------------"
-    print "('    Etot = ', f14.8, ' a.u.')", free_energy_
-    free_energies(iter) = free_energy_
-    if (iter > 3) then
-        last3 = maxval(free_energies(iter-3:iter)) - &
-            minval(free_energies(iter-3:iter))
-        if (last3 < energy_eps) then
-            nq_pos = psi**2
-            return
-        end if
-    end if
-    ! TODO: calculate the derivative together with the free energy, as pretty
-    ! much the whole calculation is shared.
-    call free_energy_derivative(nodes, elems, in, ib, Nb, Lx, Ly, Lz, xin, &
-        xiq, wtq3, T_au, &
-        nenq_pos, psi**2, phihq, dphihq, Hpsi)
-    Hpsi = Hpsi * 2*psi ! d/dpsi = 2 psi d/dn
-    mu = 1._dp / Nelec * integral(nodes, elems, wtq3, 0.5_dp * psi * Hpsi)
-    ksi_prev = ksi
-    ksi = 2*mu*psi - Hpsi
-    gamma_n = integral(nodes, elems, wtq3, ksi**2)
-    gamma_d = integral(nodes, elems, wtq3, ksi_prev**2)
-    phi = ksi + gamma_n / gamma_d * phi
-    phi_prime = phi - 1._dp / Nelec *  integral(nodes, elems, wtq3, phi * psi) * psi
-    eta = sqrt(Nelec / integral(nodes, elems, wtq3, phi_prime**2)) * phi_prime
-end do
-call stop_error("free_energy_minimization: The maximum number of iterations exceeded.")
-
-contains
-
-    real(dp) function func(theta) result(energy)
-    real(dp), intent(in) :: theta
-    psi_ = cos(theta) * psi + sin(theta) * eta
-    call free_energy(nodes, elems, in, ib, Nb, Lx, Ly, Lz, xin, xiq, wtq3, &
-        T_au, &
-        nenq_pos, psi_**2, phihq, Am_loc, phi_v, jac_det, &
-        Eh, Een, Ts, Exc, energy)
-    end function
 
 end subroutine
 
@@ -318,97 +239,6 @@ Exc = integral(nodes, elems, wtq3, exc_density * nq_pos)
 Etot = Ts + Een + Eh + Exc
 end subroutine
 
-subroutine free_energy_derivative(nodes, elems, in, ib, Nb, Lx, Ly, Lz, xin, &
-        xiq, wtq3, T_au, &
-        nenq_pos, nq_pos, phihq, dphihq, Hpsi, verbose)
-! Returns "delta F / delta n", the functional derivative with respect to the
-! density "n". Use the relation
-!     d/dpsi = 2 psi d/dn
-! to obtain the derivative with respect to psi (i.e. multiply Hpsi by 2*psi).
-real(dp), intent(in) :: nodes(:, :)
-integer, intent(in) :: elems(:, :), in(:, :, :, :), ib(:, :, :, :), Nb
-real(dp), intent(in) :: Lx, Ly, Lz, T_au
-real(dp), intent(in) :: nenq_pos(:, :, :, :), nq_pos(:, :, :, :), phihq(:, :), &
-    dphihq(:, :), xin(:), xiq(:), wtq3(:, :, :)
-logical, intent(in), optional :: verbose
-real(dp), intent(out) :: Hpsi(:, :, :, :)
-
-real(dp), allocatable, dimension(:, :, :, :) :: y, dF0dn, exc_density, &
-    nq_neutral, Venq, Vhq, nenq_neutral, Vxc
-integer, allocatable :: Ap(:), Aj(:)
-real(dp), allocatable :: Ax(:), rhs(:), sol(:), fullsol(:)
-real(dp) :: background, beta, tmp, dydn
-integer :: Nn, Ne, Nq, i, j, k, m
-logical :: verbose_
-verbose_ = .false.
-if (present(verbose)) verbose_ = verbose
-Nn = size(nodes, 2)
-Ne = size(elems, 2)
-Nq = size(xiq)
-allocate(rhs(Nb), sol(Nb), fullsol(maxval(in)), Vhq(Nq, Nq, Nq, Ne))
-allocate(y(Nq, Nq, Nq, Ne))
-allocate(dF0dn(Nq, Nq, Nq, Ne))
-allocate(exc_density(Nq, Nq, Nq, Ne))
-allocate(nq_neutral(Nq, Nq, Nq, Ne))
-allocate(Venq(Nq, Nq, Nq, Ne))
-allocate(Vxc(Nq, Nq, Nq, Ne))
-! Make the charge density net neutral (zero integral):
-background = integral(nodes, elems, wtq3, nq_pos) / (Lx*Ly*Lz)
-if (verbose_) then
-    print *, "Total (positive) electronic charge: ", background * (Lx*Ly*Lz)
-    print *, "Subtracting constant background (Q/V): ", background
-end if
-nq_neutral = nq_pos - background
-call assemble_3d(xin, nodes, elems, ib, xiq, wtq3, phihq, dphihq, &
-    4*pi*nq_neutral, Ap, Aj, Ax, rhs)
-if (verbose_) then
-    print *, "sum(rhs):    ", sum(rhs)
-    print *, "integral rhs:", integral(nodes, elems, wtq3, nq_neutral)
-    print *, "Solving..."
-end if
-!sol = solve(A, rhs)
-sol = solve_cg(Ap, Aj, Ax, rhs, zeros(size(rhs)), 1e-12_dp, 400)
-call c2fullc_3d(in, ib, sol, fullsol)
-call fe2quad_3d(elems, xin, xiq, phihq, in, fullsol, Vhq)
-
-background = integral(nodes, elems, wtq3, nenq_pos) / (Lx*Ly*Lz)
-if (verbose_) then
-    print *, "Total (negative) ionic charge: ", background * (Lx*Ly*Lz)
-    print *, "Subtracting constant background (Q/V): ", background
-end if
-nenq_neutral = nenq_pos - background
-call assemble_3d(xin, nodes, elems, ib, xiq, wtq3, phihq, dphihq, &
-    4*pi*nenq_neutral, Ap, Aj, Ax, rhs)
-if (verbose_) then
-    print *, "sum(rhs):    ", sum(rhs)
-    print *, "integral rhs:", integral(nodes, elems, wtq3, nenq_neutral)
-    print *, "Solving..."
-end if
-sol = solve_cg(Ap, Aj, Ax, rhs, zeros(size(rhs)), 1e-12_dp, 400)
-call c2fullc_3d(in, ib, sol, fullsol)
-call fe2quad_3d(elems, xin, xiq, phihq, in, fullsol, Venq)
-
-beta = 1/T_au
-y    = pi**2 / sqrt(2._dp) * beta**(3._dp/2) * nq_pos
-dydn = pi**2 / sqrt(2._dp) * beta**(3._dp/2)
-if (any(y < 0)) call stop_error("Density must be positive")
-! F0 = nq_pos / beta * f(y)
-! d F0 / d n =
-dF0dn = 1 / beta * f(y) + nq_pos / beta * f(y, deriv=.true.) * dydn
-! Exchange and correlation potential
-do m = 1, Ne
-do k = 1, Nq
-do j = 1, Nq
-do i = 1, Nq
-    call xc_pz(nq_pos(i, j, k, m), tmp, Vxc(i, j, k, m))
-end do
-end do
-end do
-end do
-
-Hpsi = dF0dn + Vhq + Venq + Vxc
-end subroutine
-
 end module
 
 
@@ -424,7 +254,7 @@ use utils, only: loadtxt, stop_error
 use splines, only: spline3pars, iixmin, poly3, spline3ders
 use interp3d, only: trilinear
 implicit none
-real(dp) :: Eh, Een, Ts, Exc, Etot
+real(dp) :: Eh, Een, Ts, Exc
 integer :: p, DOF
 real(dp) :: Z, Ediff
 real(dp), allocatable :: R(:), V(:), c(:, :)
@@ -443,18 +273,6 @@ L = 2
 T_eV = 0.0862_dp
 T_au = T_ev / Ha2eV
 call free_energy_min(L, 4, 4, 4, p, T_au, nen_splines, ne, Eh, Een, Ts, Exc, DOF)
-Etot = Ts + Een + Eh + Exc
-print *, "p =", p
-print *, "DOF =", DOF
-print *, "Rcut =", Rcut
-print *, "T_au =", T_au
-print *, "Summary of energies [a.u.]:"
-print "('    Ts   = ', f14.8)", Ts
-print "('    Een  = ', f14.8)", Een
-print "('    Eee  = ', f14.8)", Eh
-print "('    Exc  = ', f14.8)", Exc
-print *, "   ---------------------"
-print "('    Etot = ', f14.8, ' a.u. = ', f14.8, ' eV')", Etot, Etot*Ha2eV
 
 contains
 
