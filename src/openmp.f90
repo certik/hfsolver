c1b3f2fc86be87bf838770e607cba2889e6f8b9c
Ondřej Čertík <ondrej.certik@gmail.com>
2014-09-10 11:57:21 -0600

    openmp: add omp_get_wtime() interface

--- src/openmp.f90
+++ src/openmp.f90
@@ -1,11 +1,16 @@
 module openmp
+use types, only: dp
 implicit none
 private
-public omp_get_thread_num
+public omp_get_thread_num, omp_get_wtime
 
 interface
 
     integer function omp_get_thread_num()
+    end function
+
+    real(dp) function omp_get_wtime()
+    import :: dp
     end function
 
 end interface
