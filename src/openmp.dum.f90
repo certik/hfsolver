49bdbc45023a0e755a599ed024f68a348a07cd86
Ondřej Čertík <ondrej.certik@gmail.com>
2014-11-25 15:56:14 -0700

    Interface omp_get_num_threads, omp_get_max_threads

--- src/openmp.dum.f90
+++ src/openmp.dum.f90
@@ -2,12 +2,21 @@ module openmp
 use types, only: dp
 implicit none
 private
-public omp_get_thread_num, omp_get_wtime
+public omp_get_thread_num, omp_get_num_threads, omp_get_max_threads, &
+    omp_get_wtime
 
 contains
 
     integer function omp_get_thread_num()
     omp_get_thread_num = 0
+    end function
+
+    integer function omp_get_num_threads()
+    omp_get_num_threads = 1
+    end function
+
+    integer function omp_get_max_threads()
+    omp_get_max_threads = 1
     end function
 
     real(dp) function omp_get_wtime()
--- src/openmp.f90
+++ src/openmp.f90
@@ -2,11 +2,18 @@ module openmp
 use types, only: dp
 implicit none
 private
-public omp_get_thread_num, omp_get_wtime
+public omp_get_thread_num, omp_get_num_threads, omp_get_max_threads, &
+    omp_get_wtime
 
 interface
 
     integer function omp_get_thread_num()
+    end function
+
+    integer function omp_get_num_threads()
+    end function
+
+    integer function omp_get_max_threads()
     end function
 
     real(dp) function omp_get_wtime()
