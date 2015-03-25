50c9ab5b0140b92346391e0f81c104771d45996f
Ondřej Čertík <ondrej.certik@gmail.com>
2015-03-25 14:43:05 -0600

    Improve FindLibint.cmake
    * Search for the libint2.h also in the libint2 subdirectory.
    Use libint2/libint2.h instead of libint2.h
    
    * Include the libint2 subdirectory as well
    
    Few other unrelated fixes.

--- cmake/FindLibint.cmake
+++ cmake/FindLibint.cmake
@@ -1,13 +1,21 @@
 include(LibFindMacros)
 
 libfind_include(libint2.h libint)
+if(LIBINT_INCLUDE_DIR STREQUAL "LIBINT_INCLUDE_DIR-NOTFOUND")
+    # Older versions of the library had the libint2.h inside the libint2
+    # subdirectory, so we try to find it there as well
+    libfind_include(libint2/libint2.h libint)
+endif()
+
 libfind_library(int2 libint)
 
 set(LIBINT_LIBRARIES ${INT2_LIBRARY})
-set(LIBINT_INCLUDE_DIRS ${LIBINT_INCLUDE_DIR})
+# Libint requires to include both the the libint2 subdirectory and its parent
+# directory
+set(LIBINT_INCLUDE_DIRS ${LIBINT_INCLUDE_DIR} ${LIBINT_INCLUDE_DIR}/libint2)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Libint DEFAULT_MSG
-    LIBINT_LIBRARY LIBINT_INCLUDE_DIR)
+    LIBINT_LIBRARIES LIBINT_INCLUDE_DIR)
 
 mark_as_advanced(LIBINT_INCLUDE_DIR INT2_LIBRARY)
