fc9ea0e0f6e62f9b821fce3eb741de40d96d58a8
Ondřej Čertík <ondrej.certik@gmail.com>
2015-04-04 15:21:05 -0600

    Improve BDDC cmake support
    Include the .mod file

--- cmake/FindBDDCML.cmake
+++ cmake/FindBDDCML.cmake
@@ -1,9 +1,11 @@
 include(LibFindMacros)
 
+libfind_include(bddcml_interface_fortran.mod bddcml)
 libfind_library(bddcml bddcml)
 
+set(BDDCML_INCLUDE_DIRS ${BDDCML_INCLUDE_DIR})
 set(BDDCML_LIBRARIES ${BDDCML_LIBRARY})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(BDDCML DEFAULT_MSG BDDCML_LIBRARIES)
-mark_as_advanced(BDDCML_LIBRARY)
+find_package_handle_standard_args(BDDCML DEFAULT_MSG BDDCML_LIBRARY BDDCML_INCLUDE_DIR)
+mark_as_advanced(BDDCML_LIBRARY BDDCML_INCLUDE_DIR)
