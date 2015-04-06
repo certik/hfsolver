8ad5ee95ce6a7d7dbf345f8c3003c125bffa93fa
Ondřej Čertík <ondrej.certik@gmail.com>
2015-04-06 15:07:45 -0600

    Introduce MPI_HAS_FINALIZE and use it

---
+++ src/mpi_interface2.f90
@@ -0,0 +1,16 @@
+module mpi_interface
+use types, only: dp
+use mpi
+implicit none
+
+interface
+    subroutine mpi_bcast(buffer, count_, datatype, root, comm, ierr)
+    import :: dp
+    integer, intent(in) :: count_
+    real(dp), intent(inout) :: buffer(count_)
+    integer, intent(in) :: datatype, root, comm
+    integer, intent(out) :: ierr
+    end subroutine
+end interface
+
+end module
