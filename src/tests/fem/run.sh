174249c062ebb0fa1091ccd87172d7ac3ef60aba
Ondřej Čertík <ondrej.certik@gmail.com>
2015-06-26 17:54:17 -0600

    Polish the run script

--- src/tests/fem/run.sh
+++ src/tests/fem/run.sh
@@ -1,16 +1,16 @@
 #! /usr/bin/env bash
 #
-# How to setup the job, starting in the hfsolver directory where run.sh is:
+# How to setup the job, starting in the hfsolver root directory:
 #
 # HFSOLVER_DIR=`pwd`
 # mkdir -p /scratch/certik/hfsolver/run1
 # cd /scratch/certik/hfsolver/run1
-# cp $HFSOLVER_DIR/src/tests/fem/test_free_energy_bddc .
 # cp $HFSOLVER_DIR/src/tests/fem/run.sh .
+# msub run.sh
 #
 # modify the run.sh script (nodes, ppn, walltime).
 #
-#MSUB -l nodes=9:ppn=3      # number of nodes and cores (ppn)
+#MSUB -l nodes=4:ppn=12     # number of nodes and cores (ppn)
 #MSUB -l walltime=0:59:00   # max duration of the job
 #MSUB -m be                 # send mail twice: begin and end
 #MSUB -j oe                 # combine stdout and stderr
@@ -39,7 +39,9 @@ set -x
 NUMPROC=${SLURM_NPROCS}
 echo "Running $NUMPROC processes."
 
-mpiexec -n $NUMPROC ./test_free_energy_bddc
+HF_DIR=/users/certik/repos/hfsolver/src/tests/fem
+
+mpiexec -n $NUMPROC $HF_DIR/test_free_energy_bddc 4 8 8 9 4 4 3
 
 date
 echo 'Done'
