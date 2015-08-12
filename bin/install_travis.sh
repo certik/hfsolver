a3b05d1b11c4e497fdcebec70b2b6901613adeeb
Ondřej Čertík <ondrej.certik@gmail.com>
2015-08-12 15:29:41 -0600

    Install more recent cmake on Travis

---
+++ bin/install_travis.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Exit on error
+set -e
+# Echo each command
+set -x
+
+# Print CPU info for debugging purposes:
+cat /proc/cpuinfo
+gfortran -v
+/usr/bin/f95 -v
+
+# Install cmake
+wget -O- http://www.cmake.org/files/v3.3/cmake-3.3.0-Linux-x86_64.tar.gz | tar xz
+export PATH="`pwd`/cmake-3.3.0-Linux-x86_64/bin/:$PATH"
+
+cmake --version
+cmake -DCMAKE_INSTALL_PREFIX="$VIRTUAL_ENV" -DWITH_LIBINT=${TEST_LIBINT} -DWITH_FFTW=${TEST_FFTW} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DWITH_PYTHON=${TEST_PYTHON} -DWITH_OPENMP=${TEST_OPENMP} .
+make
+make install
