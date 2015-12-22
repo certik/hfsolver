8c3b6a71dce1054a0c44fa73f3b0cfcfd1ad91e8
Ondřej Čertík <ondrej.certik@gmail.com>
2015-12-22 14:05:59 -0700

    Clarify a comment in converged_energies.f90

--- src/tests/fem/converged_energies.f90
+++ src/tests/fem/converged_energies.f90
@@ -8,15 +8,16 @@ use types, only: dp
 implicit none
 
 ! [Ts, Een, Eee, Exc]
-! nuclear: alpha = 6, electronic alpha = 5
+! nuclear alpha = 6, electronic alpha = 5
 real(dp), parameter :: one_gaussian(4) = [10.61904507_dp, -2.92172113_dp, &
             1.30109500_dp, -1.43805890_dp]
 
-! nuclear: alpha = 6, electronic alpha = 5, FCC
+! nuclear alpha = 6, electronic alpha = 5, FCC
 real(dp), parameter :: four_gaussians(4) = [107.03329544_dp, 1.17805865_dp, &
             20.81751993_dp, -8.98245221_dp]
 
-! nuclear: alpha = 6, electronic alpha = 5, FCC, minimized energy
+! nuclear alpha = 6, FCC, minimized energy (independent of the initial choice
+! for eletronic density)
 real(dp), parameter :: four_gaussians_min(4) = [7.702766263178_dp, &
          -0.881714314349_dp, 0.043588511375_dp, -2.662138821658_dp]
 
