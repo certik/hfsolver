5028f692db89e36f199c790c03ceaf4984adc267
Ondřej Čertík <ondrej.certik@gmail.com>
2016-02-19 15:33:31 -0700

    Load position from file

--- src/tests/fem/test_functional_derivative_fft.f90
+++ src/tests/fem/test_functional_derivative_fft.f90
@@ -19,41 +19,43 @@ real(dp) :: Eee, Een, Ts, Exc, Etot, Etot_conv
 integer :: Ng
 real(dp) :: Z
 real(dp), allocatable :: R(:), G(:, :, :, :), G2(:, :, :)
-real(dp), allocatable :: ne(:, :, :), Hn(:, :, :)
-real(dp), allocatable :: Ven0G(:, :, :), current(:,:,:,:), Xn(:,:,:,:)
+real(dp), allocatable :: ne(:, :, :), Hn(:, :, :), Ven_rad(:)
+real(dp), allocatable :: Ven0G(:, :, :), current(:,:,:,:), Xn(:,:,:,:), X(:,:)
 real(dp) :: V0
 complex(dp), allocatable, dimension(:,:,:) :: VenG, psi, psi2, psi3, psiG, tmp
 complex(dp), allocatable, dimension(:,:,:,:) :: dpsi
 real(dp) :: L, T_eV, T_au
 integer :: i, j
-integer, parameter :: natom = 4
-real(dp) :: X(3, natom), alpha_nen, mu, dt, psi_norm
+integer, parameter :: natom = 128
+real(dp) :: alpha_nen, mu, dt, psi_norm, Ediff
 integer :: cg_iter
 real(dp) :: E0, t, omega, current_avg(3), conductivity
 
-Ng = 32
+Ng = 128
 
-L = 2
-T_eV = 0.0862_dp
+L = 8.1049178668765851_dp
+T_eV = 34.5_dp
 T_au = T_ev / Ha2eV
 
 alpha_nen = 6
 
-Z = 1
-
 allocate(Ven0G(Ng, Ng, Ng), VenG(Ng, Ng, Ng), ne(Ng, Ng, Ng), Hn(Ng, Ng, Ng))
 allocate(G(Ng, Ng, Ng, 3), G2(Ng, Ng, Ng), psi(Ng, Ng, Ng))
-allocate(R(40000), psi2(Ng, Ng, Ng), psi3(Ng, Ng, Ng), psiG(Ng, Ng, Ng))
+allocate(psi2(Ng, Ng, Ng), psi3(Ng, Ng, Ng), psiG(Ng, Ng, Ng))
 allocate(dpsi(Ng, Ng, Ng, 3), current(Ng, Ng, Ng, 3), tmp(Ng, Ng, Ng))
 allocate(Xn(Ng, Ng, Ng, 3))
-R = linspace(1._dp/40000, 0.9_dp, 40000)
+print *, "Load initial position"
+call loadtxt("../pos.txt", X)
+call assert(size(X, 1) == 3)
+call assert(size(X, 2) == natom)
 print *, "Radial nuclear potential FFT"
-call radial_potential_fourier(R, Z*erf(alpha_nen*R)/R, L, Z, Ven0G, V0)
+call read_pseudo("D.pseudo", R, Ven_rad, Z, Ediff)
+call radial_potential_fourier(R, Ven_rad, L, Z, Ven0G, V0)
 print *, "    Done."
 
 call real_space_vectors(L, Xn)
 call reciprocal_space_vectors(L, G, G2)
-call positions_fcc(X, L)
+!call positions_fcc(X, L)
 VenG = 0
 do i = 1, natom
     VenG = VenG - Ven0G * exp(-i_ * &
@@ -66,7 +68,7 @@ ne = natom / L**3
 call free_energy_min(real(natom, dp), natom, L, G2, T_au, VenG, ne, &
     1e-12_dp, Eee, Een, Ts, Exc, Etot, cg_iter)
 
-Etot_conv = sum(four_gaussians_min)
+Etot_conv = -204.88460758_dp
 print *, "Summary of energies [a.u.]:"
 print "('    Ts   = ', f14.8)", Ts
 print "('    Een  = ', f14.8)", Een
@@ -75,16 +77,8 @@ print "('    Exc  = ', f14.8)", Exc
 print *, "   ---------------------"
 print "('    Etot = ', f14.8, ' a.u. = ', f14.8, ' eV')", Etot, Etot*Ha2eV
 print *, "Errors:"
-print *, abs(Ts - four_gaussians_min(1))
-print *, abs(Een - four_gaussians_min(2))
-print *, abs(Eee - four_gaussians_min(3))
-print *, abs(Exc - four_gaussians_min(4))
 print *, abs(Etot - Etot_conv)
-call assert(abs(Ts - four_gaussians_min(1)) < 1e-7_dp)
-call assert(abs(Een - four_gaussians_min(2)) < 1e-7_dp)
-call assert(abs(Eee - four_gaussians_min(3)) < 1e-8_dp)
-call assert(abs(Exc - four_gaussians_min(4)) < 1e-8_dp)
-call assert(abs(Etot - Etot_conv) < 1e-10_dp)
+call assert(abs(Etot - Etot_conv) < 1e-8_dp)
 
 call free_energy(L, G2, T_au, VenG, ne, Eee, Een, Ts, Exc, Etot, Hn, &
     calc_value=.true., calc_derivative=.true.)
