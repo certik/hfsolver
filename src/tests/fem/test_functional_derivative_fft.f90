434e4e3ad941c875c7fc4faa17830d445735341f
Ondřej Čertík <ondrej.certik@gmail.com>
2016-02-22 17:19:38 -0700

    Works so far

--- src/tests/fem/test_functional_derivative_fft.f90
+++ src/tests/fem/test_functional_derivative_fft.f90
@@ -12,42 +12,44 @@ use constants, only: Ha2eV
 use utils, only: loadtxt, stop_error, assert, linspace
 use splines, only: spline3pars, iixmin, poly3, spline3ders
 use interp3d, only: trilinear
-use md, only: positions_fcc
-use converged_energies, only: four_gaussians_min
+use md, only: positions_fcc, positions_bcc
 implicit none
 real(dp) :: Eee, Een, Ts, Exc, Etot, Etot_conv
 integer :: Ng
 real(dp) :: Z
-real(dp), allocatable :: R(:), G(:, :, :, :), G2(:, :, :)
-real(dp), allocatable :: ne(:, :, :), Hn(:, :, :), Ven_rad(:)
+real(dp), allocatable :: R(:), G(:, :, :, :), G2(:, :, :), psi(:,:,:)
+real(dp), allocatable :: ne(:, :, :), Hn(:, :, :), Hn2(:,:,:), Ven_rad(:)
 real(dp), allocatable :: Ven0G(:, :, :), current(:,:,:,:), Xn(:,:,:,:), X(:,:)
 real(dp) :: V0
-complex(dp), allocatable, dimension(:,:,:) :: VenG, psi, psi2, psi3, psiG, tmp
+complex(dp), allocatable, dimension(:,:,:) :: VenG, tmp
 complex(dp), allocatable, dimension(:,:,:,:) :: dpsi
 real(dp) :: L, T_eV, T_au
-integer :: i, j, u
-integer, parameter :: natom = 128
+integer :: i, u
+integer :: natom = 4
 real(dp) :: alpha_nen, mu, dt, psi_norm, Ediff
 integer :: cg_iter
-real(dp) :: E0, t, omega, current_avg(3), conductivity
+real(dp) :: E0, t, omega
 
-Ng = 128
+Ng = 16
 
-L = 8.1049178668765851_dp
+!L = 8.1049178668765851_dp
+L = 1._dp
 T_eV = 34.5_dp
 T_au = T_ev / Ha2eV
 
 alpha_nen = 6
 
 allocate(Ven0G(Ng, Ng, Ng), VenG(Ng, Ng, Ng), ne(Ng, Ng, Ng), Hn(Ng, Ng, Ng))
+allocate(Hn2(Ng, Ng, Ng))
 allocate(G(Ng, Ng, Ng, 3), G2(Ng, Ng, Ng), psi(Ng, Ng, Ng))
-allocate(psi2(Ng, Ng, Ng), psi3(Ng, Ng, Ng), psiG(Ng, Ng, Ng))
 allocate(dpsi(Ng, Ng, Ng, 3), current(Ng, Ng, Ng, 3), tmp(Ng, Ng, Ng))
 allocate(Xn(Ng, Ng, Ng, 3))
 print *, "Load initial position"
-call loadtxt("../pos.txt", X)
-call assert(size(X, 1) == 3)
-call assert(size(X, 2) == natom)
+!call loadtxt("../pos.txt", X)
+!call assert(size(X, 1) == 3)
+!call assert(size(X, 2) == natom)
+allocate(X(3, natom))
+call positions_fcc(X, L)
 print *, "Radial nuclear potential FFT"
 call read_pseudo("D.pseudo", R, Ven_rad, Z, Ediff)
 call radial_potential_fourier(R, Ven_rad, L, Z, Ven0G, V0)
@@ -55,7 +57,6 @@ print *, "    Done."
 
 call real_space_vectors(L, Xn)
 call reciprocal_space_vectors(L, G, G2)
-!call positions_fcc(X, L)
 VenG = 0
 do i = 1, natom
     VenG = VenG - Ven0G * exp(-i_ * &
@@ -78,7 +79,7 @@ print *, "   ---------------------"
 print "('    Etot = ', f14.8, ' a.u. = ', f14.8, ' eV')", Etot, Etot*Ha2eV
 print *, "Errors:"
 print *, abs(Etot - Etot_conv)
-call assert(abs(Etot - Etot_conv) < 1e-8_dp)
+!call assert(abs(Etot - Etot_conv) < 1e-8_dp)
 
 call free_energy(L, G2, T_au, VenG, ne, Eee, Een, Ts, Exc, Etot, Hn, &
     calc_value=.true., calc_derivative=.true.)
@@ -104,14 +105,13 @@ psi = sqrt(ne)
 
 t = 0
 
-psi2 = psi
-psi = psi2 - i_*dt*Hn*psi2
+psi = psi - dt*Hn*psi
 
-ne = real(psi*conjg(psi), dp)
+ne = psi**2
 psi_norm = integral(L, ne)
 print *, "Initial norm of psi:", psi_norm
 psi = sqrt(natom / psi_norm) * psi
-ne = real(psi*conjg(psi), dp)
+ne = psi**2
 psi_norm = integral(L, ne)
 print *, "norm of psi:", psi_norm
 
@@ -124,20 +124,12 @@ close(u)
 do i = 1, 100
     t = t + dt
     print *, "iter =", i, "time =", t
-    psi3 = psi2; psi2 = psi
-    psi = psi3 - 2*i_*dt*(Hn + E0*Xn(:,:,:,1)*sin(omega*t))*psi2
-    ne = real(psi*conjg(psi), dp)
-    call real2fourier(psi, psiG)
-    psiG(1,1,1) = 0
-    do j = 1, 3
-        call fourier2real(i_*G(:,:,:,j)*psiG, dpsi(:,:,:,j))
-        tmp = (conjg(psi)*dpsi(:,:,:,j)-psi*conjg(dpsi(:,:,:,j))) / (2*natom*i_)
-        if (maxval(abs(aimag(tmp))) > 1e-12_dp) then
-            print *, "INFO: current  max imaginary part:", maxval(aimag(tmp))
-        end if
-        current(:,:,:,j) = real(tmp, dp)
-    end do
-
+    psi = psi - dt*Hn*psi
+    ne = psi**2
+    psi_norm = integral(L, ne)
+    print *, "Initial norm of psi:", psi_norm
+    psi = sqrt(natom / psi_norm) * psi
+    ne = psi**2
     psi_norm = integral(L, ne)
     print *, "norm of psi:", psi_norm
 
@@ -152,18 +144,6 @@ do i = 1, 100
     print *, "   ---------------------"
     print "('    Etot = ', f14.8, ' a.u. = ', f14.8, ' eV')", Etot, Etot*Ha2eV
 
-
-    do j = 1, 3
-        current_avg(j) = integral(L, current(:, :, :, j))/L**3
-    end do
-    print *, "E field along the 'x' direction =", E0*sin(omega*t)
-    print *, "average current =", current_avg
-    print *, "current normalized =", current_avg / current_avg(1)
-    conductivity = current_avg(1) / E0*sin(omega*t)
-    print *, "conductivity along the 'x' direction =", conductivity
-    open(newunit=u, file="log.txt", position="append", status="old")
-    write(u, *) E0*sin(omega*t), current_avg, conductivity
-    close(u)
 
 
 end do
