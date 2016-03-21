ab99c957e1427265c2ebfcd99140c3297b6eefa9
Ondřej Čertík <ondrej.certik@gmail.com>
2016-03-21 16:19:43 -0600

    Read the initial condition from the input file

--- src/tests/ofmd_fft.f90
+++ src/tests/ofmd_fft.f90
@@ -3,10 +3,10 @@ use, intrinsic :: iso_fortran_env, only: output_unit
 use types, only: dp
 use constants, only: i_, K2au, density2gcm3, u2au, s2au, Ha2eV
 use md, only: velocity_verlet, positions_random, &
-                calc_min_distance, positions_fcc
+                calc_min_distance, positions_fcc, positions_bcc
 use ewald_sums, only: ewald_box
 use random, only: randn
-use utils, only: init_random, stop_error, assert, linspace, clock
+use utils, only: init_random, stop_error, assert, linspace, clock, loadtxt
 use ofdft, only: read_pseudo
 use ofdft_fft, only: free_energy_min, radial_potential_fourier, &
     reciprocal_space_vectors, real2fourier, fourier2real, logging_info
@@ -37,6 +37,7 @@ real(dp) :: Eee, Een, Ts, Exc, Etot, Enn
 real(dp), allocatable :: fnn(:, :), q(:), fen(:, :)
 integer :: dynamics, functional, Ng, Nspecies, start, Nmesh
 integer :: cg_iter
+real(dp) :: sigma
 real(dp), dimension(:,:,:,:), allocatable :: ne_aux
 character(len=10) :: atom_name
 real(dp) :: mdt1, mdt2
@@ -46,7 +47,7 @@ logging_info = .false. ! Turn of the INFO warnings
 call read_input("OFMD.input", Temp, rho, Nspecies, N, Am, atom_name, start, &
     dynamics, functional, Ng, scf_eps, steps, dt)
 call read_pseudo("fem/" // trim(atom_name) // ".pseudo", R, Ven_rad, Z, Ediff)
-allocate(X(3, N), V(3, N), f(3, N), m(N))
+allocate(V(3, N), f(3, N), m(N))
 allocate(Ven0G(Ng, Ng, Ng), VenG(Ng, Ng, Ng), ne(Ng, Ng, Ng), neG(Ng, Ng, Ng))
 allocate(G(Ng, Ng, Ng, 3), G2(Ng, Ng, Ng))
 allocate(fnn(3, N), q(N), fen(3, N))
@@ -82,10 +83,27 @@ R2 = linspace(0._dp, L/2, Nmesh)
 
 call reciprocal_space_vectors(L, G, G2)
 
-! Make it deterministic for now
-!call init_random()
-!call positions_random(X, L, 2**(1._dp/6)*sigma, 10)
-call positions_fcc(X, L)
+select case(start)
+    case (0)
+        call loadtxt("pos.txt", X)
+        call assert(size(X, 1) == 3)
+        call assert(size(X, 2) == N)
+    case (3)
+        allocate(X(3, N))
+        call positions_fcc(X, L)
+    case (4)
+        allocate(X(3, N))
+        call positions_bcc(X, L)
+    case (5)
+        ! Make it deterministic for now
+        !call init_random()
+        sigma = 1
+        allocate(X(3, N))
+        call positions_random(X, L, 2**(1._dp/6)*sigma, 10)
+    case default
+        call stop_error("Invalid initial condition.")
+end select
+
 print *, "Positions:"
 do i = 1, N
     print *, i, X(:, i)
