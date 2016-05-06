d958bdbf1578d3d9f04917a28d98e3d57719e060
Ondřej Čertík <ondrej.certik@gmail.com>
2016-05-05 19:17:08 -0600

    Implement current

--- src/tests/fft/test_sch3d_rt.f90
+++ src/tests/fft/test_sch3d_rt.f90
@@ -17,12 +17,12 @@ use sorting, only: argsort
 implicit none
 integer :: Ng
 real(dp), allocatable :: G(:,:,:,:), G2(:,:,:), ne(:,:,:)
-real(dp), allocatable :: Xn(:,:,:,:), Vn(:,:,:), r(:,:,:)
-complex(dp), allocatable :: psi(:,:,:), psiG(:,:,:)
+real(dp), allocatable :: Xn(:,:,:,:), Vn(:,:,:), r(:,:,:), current(:,:,:,:)
+complex(dp), allocatable :: psi(:,:,:), psiG(:,:,:), dpsi(:,:,:,:), tmp(:,:,:)
 integer, parameter :: nelec = 1
-real(dp) :: L, dt, t, psi_norm, E_tot
+real(dp) :: L, dt, t, psi_norm, E_tot, current_avg(3)
 real(dp) :: lambda, E0, Ex, td, tw
-integer :: i
+integer :: i, j
 
 Ng = 32
 
@@ -31,7 +31,8 @@ L = 10._dp
 allocate(G(Ng,Ng,Ng,3), G2(Ng,Ng,Ng), psi(Ng,Ng,Ng))
 allocate(Xn(Ng, Ng, Ng, 3), Vn(Ng, Ng, Ng), r(Ng, Ng, Ng))
 allocate(psiG(Ng,Ng,Ng))
-allocate(ne(Ng,Ng,Ng))
+allocate(ne(Ng,Ng,Ng), tmp(Ng,Ng,Ng))
+allocate(dpsi(Ng,Ng,Ng,3), current(Ng,Ng,Ng,3))
 
 call real_space_vectors(L, Xn)
 call reciprocal_space_vectors(L, G, G2)
@@ -103,9 +104,24 @@ do i = 1, 50
     psi_norm = integral(L, ne)
     print *, "norm of psi:", psi_norm
 
+    ! Total energy
     call real2fourier(psi, psiG)
     E_tot = 1._dp/2 * integralG(G2*abs(psiG)**2, L) + integral(L, Vn*ne)
     print *, "E_tot       =", E_tot
+
+    ! Current
+    do j = 1, 3
+        call fourier2real(i_*G(:,:,:,j)*psiG, dpsi(:,:,:,j))
+        tmp = (conjg(psi)*dpsi(:,:,:,j)-psi*conjg(dpsi(:,:,:,j))) / (2*nelec*i_)
+        call assert(maxval(abs(aimag(tmp))) < epsilon(1._dp))
+        current(:,:,:,j) = real(tmp, dp)
+    end do
+    do j = 1, 3
+        current_avg(j) = integral(L, current(:, :, :, j))/L**3
+    end do
+    print *, "average current X =", current_avg(1)
+    print *, "average current Y =", current_avg(2)
+    print *, "average current Z =", current_avg(3)
 
 end do
 
