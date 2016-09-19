3219c16b75d5bb374f3a578d1fb90fbc02822197
Ondřej Čertík <ondrej.certik@gmail.com>
2016-09-19 00:41:57 -0600

    Refactor, polish

--- src/tests/test_ffte_par.f90
+++ src/tests/test_ffte_par.f90
@@ -9,17 +9,23 @@ use ffte, only: ffte_fft3_inplace => fft3_inplace, &
 use openmp, only: omp_get_wtime
 use mpi_interface, only: mpi_finalize, MPI_COMM_WORLD, mpi_comm_rank, &
     mpi_comm_size, mpi_init, mpi_comm_split, MPI_INTEGER, mpi_bcast_ints, &
-    mpi_barrier
+    mpi_barrier, MPI_DOUBLE_PRECISION, MPI_SUM
+use ofdft_fft, only: reciprocal_space_vectors, real_space_vectors
 implicit none
 
 complex(dp), dimension(:,:,:), allocatable :: x3, x3d, x3d2, x3d3
-real(dp) :: tmp
-integer :: l, m, n, i, j, k
+real(dp), allocatable :: G2(:,:,:), X(:,:,:,:), X_global(:,:,:,:)
+real(dp), allocatable :: G_global(:,:,:,:), G2_global(:,:,:)
+real(dp) :: L, r2
+integer :: i, j, k
 integer :: Ng(3)
 real(dp) :: t1, t2, t3
 
 !  parallel variables
-integer :: myid, comm_all, commy, commz, nproc, ierr, nsub(3), Ng_local(3)
+integer :: comm_all, commy, commz, nproc, ierr, nsub(3), Ng_local(3)
+integer :: myid ! my ID (MPI rank), starts from 0
+integer :: myxyz(3) ! myid, converted to the (x, y, z) box, starts from 0
+integer :: ijk_global(3)
 
 call mpi_init(ierr)
 comm_all  = MPI_COMM_WORLD
@@ -51,11 +57,14 @@ if (myid == 0) then
 end if
 call mpi_bcast_ints(nsub, size(nsub), MPI_INTEGER, 0, comm_all, ierr)
 
-call mpi_comm_split(comm_all, myid/nsub(2), 0, commy, ierr)
-call mpi_comm_split(comm_all, mod(myid, nsub(2)), 0, commz, ierr)
+myxyz = [0, myid/nsub(3), mod(myid, nsub(3))]
+
+call mpi_comm_split(comm_all, myxyz(2), 0, commy, ierr)
+call mpi_comm_split(comm_all, myxyz(3), 0, commz, ierr)
 
 Ng = 32
 Ng_local = Ng / nsub
+L = 10
 
 if (myid == 0) then
     print *, "Ng:      ", Ng
@@ -70,20 +79,43 @@ allocate(x3(Ng_local(1), Ng_local(2), Ng_local(3)))
 allocate(x3d(Ng_local(1), Ng_local(2), Ng_local(3)))
 allocate(x3d2(Ng_local(1), Ng_local(2), Ng_local(3)))
 allocate(x3d3(Ng_local(1), Ng_local(2), Ng_local(3)))
+allocate(G_global(Ng(1), Ng(2), Ng(3), 3))
+allocate(G2_global(Ng(1), Ng(2), Ng(3)))
+allocate(X_global(Ng(1), Ng(2), Ng(3), 3))
+allocate(G2(Ng_local(1), Ng_local(2), Ng_local(3)))
+allocate(X(Ng_local(1), Ng_local(2), Ng_local(3), 3))
+call real_space_vectors(L, X_global)
+call reciprocal_space_vectors(L, G_global, G2_global)
+! Convert X_global to X (local)
+! Convert G2_global to G2 (local)
 do k = 1, size(x3, 3)
 do j = 1, size(x3, 2)
 do i = 1, size(x3, 1)
-    call random_number(tmp)
-    x3(i, j, k) = tmp
+    ijk_global = [i, j, k] + myxyz*Ng_local
+    X(i,j,k,:) = X_global(ijk_global(1), ijk_global(2), ijk_global(3), :)
+    G2(i,j,k) = G2_global(ijk_global(1), ijk_global(2), ijk_global(3))
 end do
 end do
 end do
+
+do k = 1, size(x3, 3)
+do j = 1, size(x3, 2)
+do i = 1, size(x3, 1)
+    r2 =sum((X(i,j,k,:)-[L/2,L/2,L/2])**2)
+    x3(i, j, k) = exp(-r2)
+end do
+end do
+end do
+
+print *, "integral:", myid, pintegral(comm_all, L, real(x3, dp), Ng)
 x3d = x3
 call pfft3_init(Ng)
 call cpu_time(t1)
 call pfft3(x3d, x3d2, commy, commz, Ng, nsub)
+x3d2 = x3d2 / product(Ng)
 call cpu_time(t2)
 call pifft3(x3d2, x3d3, commy, commz, Ng, nsub)
+x3d3 = x3d3 * product(Ng)
 call cpu_time(t3)
 if (myid == 0) then
     print *, myid, maxval(abs(x3 - x3d3))
@@ -92,8 +124,41 @@ if (myid == 0) then
     print *, t3-t2, (t3-t2)*nproc
 end if
 call assert(all(abs(x3 - x3d3) < 5e-15_dp))
+
+if (myid == 0) then
+    x3d2(1,1,1) = 0
+end if
+x3d2 = x3d2/G2 ! FIXME: this depends on the decomposition...
+! Probably x3d2 is distributed differently than G2 for some reason.
+print *, "integralG:", myid, pintegralG(comm_all, L, x3d2)
 deallocate(x3, x3d, x3d2, x3d3)
 
 call mpi_finalize(ierr)
+
+contains
+
+real(dp) function pintegral(comm, L, f, Ng) result(r)
+! Calculates the integral over 'f' in parallel, returns the answer on all
+! processors.
+integer, intent(in) :: comm
+real(dp), intent(in) :: L, f(:,:,:)
+integer, intent(in) :: Ng(:)
+real(dp) :: myr
+myr = sum(f)
+call mpi_allreduce(myr, r, 1, MPI_DOUBLE_PRECISION, MPI_SUM, comm, ierr)
+r = r * L**3 / product(Ng)
+end function
+
+real(dp) function pintegralG(comm, L, fG) result(r)
+! Calculates the integral over 'fG' in reciprocal space in parallel, returns
+! the answer on all processors.
+integer, intent(in) :: comm
+real(dp), intent(in) :: L
+complex(dp), intent(in) :: fG(:, :, :)
+real(dp) :: myr
+myr = sum(real(fG, dp))
+call mpi_allreduce(myr, r, 1, MPI_DOUBLE_PRECISION, MPI_SUM, comm, ierr)
+r = r * L**3
+end function
 
 end program
