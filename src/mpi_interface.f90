5395ce7f054bfb7a4af6b25d8c2b0f4c39b28261
Ondřej Čertík <ondrej.certik@gmail.com>
2016-09-18 21:40:54 -0600

    Implement interface to mpi_bcast

--- src/mpi_interface.f90
+++ src/mpi_interface.f90
@@ -7,14 +7,40 @@ interface
     subroutine mpi_finalize(ierr)
     integer, intent(out) :: ierr
     end subroutine
-
-    subroutine mpi_bcast(buffer, count_, datatype, root, comm, ierr)
-    import :: dp
-    integer, intent(in) :: count_
-    real(dp), intent(inout) :: buffer(count_)
-    integer, intent(in) :: datatype, root, comm
-    integer, intent(out) :: ierr
-    end subroutine
 end interface
+
+contains
+
+subroutine mpi_bcast_floats(buffer, count_, datatype, root, comm, ierr)
+integer, intent(in) :: count_
+real(dp), intent(inout) :: buffer(count_)
+integer, intent(in) :: datatype, root, comm
+integer, intent(out) :: ierr
+call mpi_bcast(buffer, count_, datatype, root, comm, ierr)
+end subroutine
+
+subroutine mpi_bcast_float(buffer, count_, datatype, root, comm, ierr)
+integer, intent(in) :: count_
+real(dp), intent(inout) :: buffer
+integer, intent(in) :: datatype, root, comm
+integer, intent(out) :: ierr
+call mpi_bcast(buffer, count_, datatype, root, comm, ierr)
+end subroutine
+
+subroutine mpi_bcast_ints(buffer, count_, datatype, root, comm, ierr)
+integer, intent(in) :: count_
+integer, intent(inout) :: buffer(count_)
+integer, intent(in) :: datatype, root, comm
+integer, intent(out) :: ierr
+call mpi_bcast(buffer, count_, datatype, root, comm, ierr)
+end subroutine
+
+subroutine mpi_bcast_int(buffer, count_, datatype, root, comm, ierr)
+integer, intent(in) :: count_
+integer, intent(inout) :: buffer
+integer, intent(in) :: datatype, root, comm
+integer, intent(out) :: ierr
+call mpi_bcast(buffer, count_, datatype, root, comm, ierr)
+end subroutine
 
 end module
