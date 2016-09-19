f7c2eb7f0ac6b1114f9243b5dc54ea19762caf4c
Ondřej Čertík <ondrej.certik@gmail.com>
2016-09-19 13:40:43 -0600

    Fix the code
    Put proper mpi_barriers in the timings.

--- src/tests/test_ffte_par.f90
+++ src/tests/test_ffte_par.f90
@@ -1,5 +1,6 @@
 program test_ffte_par
 use types, only: dp
+use constants, only: pi
 use fourier, only: dft, idft, fft, fft_vectorized, fft_pass, fft_pass_inplace, &
         fft_vectorized_inplace, calculate_factors, ifft_pass, fft2_inplace, &
         fft3_inplace, ifft3_inplace
@@ -10,13 +11,14 @@ use openmp, only: omp_get_wtime
 use mpi2, only: mpi_finalize, MPI_COMM_WORLD, mpi_comm_rank, &
     mpi_comm_size, mpi_init, mpi_comm_split, MPI_INTEGER, &
     mpi_barrier, MPI_DOUBLE_PRECISION, MPI_SUM, mpi_bcast, mpi_allreduce
-use ofdft_fft, only: reciprocal_space_vectors, real_space_vectors
+use ofdft_fft, only: reciprocal_space_vectors, real_space_vectors, &
+    real2fourier
 implicit none
 
 complex(dp), dimension(:,:,:), allocatable :: x3, x3d, x3d2, x3d3
 real(dp), allocatable :: G2(:,:,:), X(:,:,:,:), X_global(:,:,:,:)
 real(dp), allocatable :: G_global(:,:,:,:), G2_global(:,:,:)
-real(dp) :: L, r2
+real(dp) :: L(3), r2, alpha, Z, Eee, Eee_conv
 integer :: i, j, k
 integer :: Ng(3)
 real(dp) :: t1, t2, t3
@@ -38,19 +40,32 @@ if (myid == 0) then
         nsub(3) = (2**(LNPU(1)/2))*(3**(LNPU(2)/2))*(5**(LNPU(3)/2))
         nsub(2) = nproc / nsub(3)
         nsub(1) = 1
+        Ng = 32
+        L = 2
     else
-        if (command_argument_count() /= 3) then
+        if (command_argument_count() /= 9) then
             print *, "Usage:"
             print *,
-            print *, "test_ffte_par nsubx nsuby nsubz"
+            print *, "test_ffte_par L(3) Ng(3) nsub(3)"
             call stop_error("Incorrect number of arguments.")
         end if
-        nsub(1) = get_int_arg(1)
-        nsub(2) = get_int_arg(2)
-        nsub(3) = get_int_arg(3)
+        L(1) = get_int_arg(1)
+        L(2) = get_int_arg(2)
+        L(3) = get_int_arg(3)
+        Ng(1) = get_int_arg(4)
+        Ng(2) = get_int_arg(5)
+        Ng(3) = get_int_arg(6)
+        nsub(1) = get_int_arg(7)
+        nsub(2) = get_int_arg(8)
+        nsub(3) = get_int_arg(9)
     end if
-    print *, "nproc:", nproc
-    print *, "nsub:", nsub
+    Ng_local = Ng / nsub
+
+    print *, "L:       ", L
+    print *, "nproc:   ", nproc
+    print *, "nsub:    ", nsub
+    print *, "Ng:      ", Ng
+    print *, "Ng_local:", Ng_local
 
     if (product(nsub) /= nproc) then
         call stop_error("nproc must be equal to the number of subdomains")
@@ -58,28 +73,20 @@ if (myid == 0) then
     if (nsub(1) /= 1) then
         call stop_error("nsub(1) must be equal to 1")
     end if
-end if
-call mpi_bcast(nsub, size(nsub), MPI_INTEGER, 0, comm_all, ierr)
-
-myxyz = [0, myid/nsub(2), mod(myid, nsub(2))]
-
-call mpi_comm_split(comm_all, myxyz(2), 0, commy, ierr)
-call mpi_comm_split(comm_all, myxyz(3), 0, commz, ierr)
-
-myxyz = [0, mod(myid, nsub(2)), myid/nsub(2)]
-
-Ng = 32
-Ng_local = Ng / nsub
-L = 10
-
-if (myid == 0) then
-    print *, "Ng:      ", Ng
-    print *, "Ng_local:", Ng_local
     if (.not. all(Ng_local * nsub == Ng)) then
         call stop_error("Ng must be equal to Ng_local * nsub")
     end if
 end if
-call mpi_barrier(comm_all, ierr)
+call mpi_bcast(L, size(L), MPI_DOUBLE_PRECISION, 0, comm_all, ierr)
+call mpi_bcast(nsub, size(nsub), MPI_INTEGER, 0, comm_all, ierr)
+call mpi_bcast(Ng, size(Ng), MPI_INTEGER, 0, comm_all, ierr)
+call mpi_bcast(Ng_local, size(Ng_local), MPI_INTEGER, 0, comm_all, ierr)
+
+myxyz = [0, mod(myid, nsub(2)), myid/nsub(2)]
+
+call mpi_comm_split(comm_all, myxyz(3), 0, commy, ierr)
+call mpi_comm_split(comm_all, myxyz(2), 0, commz, ierr)
+
 
 allocate(x3(Ng_local(1), Ng_local(2), Ng_local(3)))
 allocate(x3d(Ng_local(1), Ng_local(2), Ng_local(3)))
@@ -98,45 +105,65 @@ do k = 1, size(x3, 3)
 do j = 1, size(x3, 2)
 do i = 1, size(x3, 1)
     ijk_global = [i, j, k] + myxyz*Ng_local
+!    print "(7i0)", myid, i, j, k, ijk_global
     X(i,j,k,:) = X_global(ijk_global(1), ijk_global(2), ijk_global(3), :)
     G2(i,j,k) = G2_global(ijk_global(1), ijk_global(2), ijk_global(3))
 end do
 end do
 end do
 
+! Setup two Gaussians with opposite charges, thus overall the density is net
+! neutral:
+alpha = 5
+Z = 3
 do k = 1, size(x3, 3)
 do j = 1, size(x3, 2)
 do i = 1, size(x3, 1)
-    r2 =sum((X(i,j,k,:)-[L/2,L/2,L/2])**2)
-    x3(i, j, k) = exp(-r2)
+    r2 =sum((X(i,j,k,:)-L/2)**2)
+    x3(i, j, k) = Z*alpha**3/pi**(3._dp/2)*exp(-alpha**2*r2) &
+        - Z*(alpha+1)**3/pi**(3._dp/2)*exp(-(alpha+1)**2*r2)
 end do
 end do
 end do
 
-print *, "integral:", myid, pintegral(comm_all, L, real(x3, dp), Ng)
+Eee = pintegral(comm_all, L, real(x3, dp), Ng)
+if (myid == 0) then
+    print *, "integral:", myid, Eee
+    call assert(abs(Eee) < 1e-10_dp)
+end if
 x3d = x3
 call pfft3_init(Ng)
+call mpi_barrier(comm_all, ierr)
 call cpu_time(t1)
 call pfft3(x3d, x3d2, commy, commz, Ng, nsub)
-x3d2 = x3d2 / product(Ng)
+call mpi_barrier(comm_all, ierr)
+!call real2fourier(x3d, x3d2)
 call cpu_time(t2)
-call pifft3(x3d2, x3d3, commy, commz, Ng, nsub)
-x3d3 = x3d3 * product(Ng)
+x3d2 = x3d2 / product(Ng)
+!call pifft3(x3d2, x3d3, commy, commz, Ng, nsub)
+!x3d3 = x3d3 * product(Ng)
 call cpu_time(t3)
 if (myid == 0) then
-    print *, myid, maxval(abs(x3 - x3d3))
+!    print *, myid, maxval(abs(x3 - x3d3))
     print *, "Timings (t, t*nproc):"
     print *, t2-t1, (t2-t1)*nproc
-    print *, t3-t2, (t3-t2)*nproc
+!    print *, t3-t2, (t3-t2)*nproc
 end if
-call assert(all(abs(x3 - x3d3) < 5e-15_dp))
+!call assert(all(abs(x3 - x3d3) < 5e-15_dp))
 
+x3d3 = 4*pi*x3d2/G2
 if (myid == 0) then
-    x3d2(1,1,1) = 0
+    x3d3(1,1,1) = 0
 end if
-x3d2 = x3d2/G2 ! FIXME: this depends on the decomposition...
-! Probably x3d2 is distributed differently than G2 for some reason.
-print *, "integralG:", myid, pintegralG(comm_all, L, x3d2)
+Eee = pintegralG(comm_all, L, real(x3d3*conjg(x3d2), dp)) / 2 &
+    - Z**2*alpha/sqrt(2*pi)
+!Eee = pintegralG(comm_all, L, abs(x3d2)**2/G2)
+if (myid == 0) then
+    print *, "integralG:", myid, Eee
+    Eee_conv = -17.465136801093962_dp
+    print *, "error:", abs(Eee-Eee_conv)
+    call assert(abs(Eee - Eee_conv) < 1e-12_dp)
+end if
 deallocate(x3, x3d, x3d2, x3d3)
 
 call mpi_finalize(ierr)
@@ -147,24 +174,25 @@ real(dp) function pintegral(comm, L, f, Ng) result(r)
 ! Calculates the integral over 'f' in parallel, returns the answer on all
 ! processors.
 integer, intent(in) :: comm
-real(dp), intent(in) :: L, f(:,:,:)
+real(dp), intent(in) :: L(:), f(:,:,:)
 integer, intent(in) :: Ng(:)
 real(dp) :: myr
 myr = sum(f)
 call mpi_allreduce(myr, r, 1, MPI_DOUBLE_PRECISION, MPI_SUM, comm, ierr)
-r = r * L**3 / product(Ng)
+r = r * product(L/Ng)
 end function
 
 real(dp) function pintegralG(comm, L, fG) result(r)
 ! Calculates the integral over 'fG' in reciprocal space in parallel, returns
 ! the answer on all processors.
 integer, intent(in) :: comm
-real(dp), intent(in) :: L
-complex(dp), intent(in) :: fG(:, :, :)
+real(dp), intent(in) :: L(:)
+real(dp), intent(in) :: fG(:, :, :)
 real(dp) :: myr
-myr = sum(real(fG, dp))
+!myr = sum(real(fG, dp))
+myr = sum(fG)
 call mpi_allreduce(myr, r, 1, MPI_DOUBLE_PRECISION, MPI_SUM, comm, ierr)
-r = r * L**3
+r = r * product(L)
 end function
 
 end program
