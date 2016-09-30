2d1ff47f1dda84149fdaebd089302ec771896bce
Ondřej Čertík <ondrej.certik@gmail.com>
2016-09-29 20:43:26 -0600

    Travis: allow openmpi to oversubscribe
    Without losing performance.

--- bin/test_travis.sh
+++ bin/test_travis.sh
@@ -12,6 +12,7 @@ else
     if [[ "${TEST_SLOW}" == "yes" ]]; then
       ctest -V --output-on-failure -L slow
     elif [[ "${TEST_MPI}" == "yes" ]]; then
+      export OMPI_MCA_mpi_yield_when_idle=1
       ctest --output-on-failure -L mpi
     else
       ctest --output-on-failure -LE slow
