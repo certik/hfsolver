d5275211e531a4317b44b847d86d34728802b0b3
Ondřej Čertík <ondrej.certik@gmail.com>
2017-04-02 10:21:54 -0600

    Wrap parpack and add a test
    The test compares IT and Arpack.

--- cmake/FindARPACK.cmake
+++ cmake/FindARPACK.cmake
@@ -2,10 +2,12 @@ find_package(Lapack REQUIRED)
 
 
 find_library(ARPACK_LIBRARY NAMES arpack)
+find_library(PARPACK_LIBRARY NAMES parpack)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(ARPACK DEFAULT_MSG ARPACK_LIBRARY)
+find_package_handle_standard_args(ARPACK DEFAULT_MSG ARPACK_LIBRARY
+    PARPACK_LIBRARY)
 
 add_library(p::arpack INTERFACE IMPORTED)
 set_property(TARGET p::arpack PROPERTY INTERFACE_LINK_LIBRARIES
-    ${ARPACK_LIBRARY} ${LAPACK_LIBRARIES})
+    ${ARPACK_LIBRARY} ${PARPACK_LIBRARY} ${LAPACK_LIBRARIES})
