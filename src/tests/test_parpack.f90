d46630d25666f5b88c4e1213ec8bee8f7761686f
Ondřej Čertík <ondrej.certik@gmail.com>
2017-04-04 17:37:20 -0600

    Compute || A*x - lambda*x ||

--- src/tests/test_parpack.f90
+++ src/tests/test_parpack.f90
@@ -4,14 +4,15 @@ use types, only: dp
 use utils, only: stop_error, assert
 use mpi2, only: mpi_finalize, MPI_COMM_WORLD, mpi_comm_rank, &
     mpi_comm_size, mpi_init, mpi_recv, mpi_send, MPI_DOUBLE_PRECISION, &
-    MPI_STATUS_SIZE
+    MPI_STATUS_SIZE, MPI_SUM, mpi_allreduce
 use arpack, only: peig
 implicit none
 
 integer :: n, nloc, nev, ncv, i
-real(dp), allocatable :: d(:), v(:,:)
+real(dp), allocatable :: d(:), v(:,:), tmp(:)
 real(dp), parameter :: e_ref(4) =  [5.4780178344413200_dp, &
     5.7004342714592289_dp, 5.8649444588087105_dp, 5.9659461993678038_dp]
+real(dp) :: r
 
 integer :: comm, myid, nproc, ierr
 
@@ -26,10 +27,16 @@ ncv = 8
 nloc = n / nproc ! local vector dimension
 call assert(nloc*nproc == n)
 allocate(v(nloc,ncv), d(ncv))
+allocate(tmp(nloc))
 call peig(comm, myid, nloc, nev, ncv, "LM", av, d, v)
+if (myid == 0) print *, "  lambda                    error                     ||A*x-lambda*x||"
 do i = 1, nev
-    if (myid == 0) print *, d(i), abs(d(i) - e_ref(i))
-!    if (abs(d(i) - e_ref(i)) > 1e-14_dp) call stop_error("Error in ref. check")
+    ! Compute || A*x - lambda*x ||
+    call av(v(:,i), tmp) ! A*x
+    tmp = tmp - d(i)*v(:,i) ! - lambda*x
+    r = sqrt(psum(comm, tmp**2)) ! l2 norm
+    if (myid == 0) print *, d(i), abs(d(i) - e_ref(i)), r
+    if (abs(d(i) - e_ref(i)) > 1e-13_dp) call stop_error("Error in ref. check")
 end do
 
 if (myid == 0) print *, "Done"
@@ -73,5 +80,16 @@ contains
      y(size(y)) = y(size(y)) + du*buf
   endif
   end
+
+    real(dp) function psum(comm, f) result(r)
+    ! Calculates the sum over 'f' in parallel, returns the answer on all
+    ! processors.
+    integer, intent(in) :: comm
+    real(dp), intent(in) :: f(:)
+    real(dp) :: myr
+    integer :: ierr
+    myr = sum(f)
+    call mpi_allreduce(myr, r, 1, MPI_DOUBLE_PRECISION, MPI_SUM, comm, ierr)
+    end function
 
 end
