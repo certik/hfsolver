ba0c457b4d38f6f11909856cad653d1bc2f3d765
Ondřej Čertík <ondrej.certik@gmail.com>
2017-04-06 16:25:38 -0600

    Try an H2 example

--- src/tests/fft/test_vloc.f90
+++ src/tests/fft/test_vloc.f90
@@ -1,6 +1,6 @@
 program test_psch3d
 use types, only: dp
-use constants, only: Ha2eV, density2gcm3, u2au, s2au, K2au
+use constants, only: Ha2eV, density2gcm3, u2au, s2au, K2au, i_, pi
 use fourier, only: dft, idft, fft, fft_vectorized, fft_pass, fft_pass_inplace, &
         fft_vectorized_inplace, calculate_factors, ifft_pass, fft2_inplace, &
         fft3_inplace, ifft3_inplace
@@ -24,20 +24,20 @@ implicit none
 
 complex(dp), dimension(:,:,:), allocatable :: neG, psiG, psi, tmp
 real(dp), dimension(:,:,:), allocatable :: G2, Hn, Htot, HtotG, Ven0G, ne, Vloc
-real(dp), allocatable :: G(:,:,:,:), X(:,:,:,:), Xion(:,:), R(:,:,:), &
+real(dp), allocatable :: G(:,:,:,:), X(:,:,:,:), Xion(:,:), &
     current(:,:,:,:), eigs(:), orbitals(:,:,:,:), eigs_ref(:), occ(:), &
     Vee(:,:,:), V_old(:,:,:), V_new(:,:,:), exc(:,:,:), Vxc(:,:,:)
-complex(dp), allocatable :: dpsi(:,:,:,:), VeeG(:,:,:)
-real(dp) :: L(3)
+complex(dp), allocatable :: dpsi(:,:,:,:), VeeG(:,:,:), VenG(:,:,:)
+real(dp) :: L(3), r
 integer :: i, j, k
 integer :: Ng(3)
 integer :: LNPU(3)
 integer :: natom
 logical :: velocity_gauge
-real(dp) :: T_eV, T_au, dt, alpha, rho, norm
+real(dp) :: T_eV, T_au, dt, alpha, rho, norm, w2
 real(dp) :: rloc, C1, C2, Zion
 real(dp), allocatable :: m(:)
-integer :: nev, ncv
+integer :: nev, ncv, na
 real(dp), parameter :: D(5) = [0.65435_dp, 2.45106_dp, -1.536643785333E-01_dp, &
     1.153664378533E+00_dp, 5.0000_dp]
 
@@ -49,7 +49,7 @@ integer :: myxyz(3) ! myid, converted to the (x, y, z) box, starts from 0
 rho = 0.01_dp / density2gcm3  ! g/cc
 T_eV = 50._dp
 T_au = T_ev / Ha2eV
-natom = 1
+natom = 2
 dt = 1e-4_dp
 allocate(m(natom))
 m = 2._dp * u2au ! Using Argon mass in atomic mass units [u]
@@ -120,7 +120,6 @@ call mpi_comm_split(comm_all, myxyz(2), 0, commz, ierr)
 allocate(ne(Ng_local(1), Ng_local(2), Ng_local(3)))
 allocate(neG(Ng_local(1), Ng_local(2), Ng_local(3)))
 call allocate_mold(G2, ne)
-call allocate_mold(R, ne)
 call allocate_mold(Hn, ne)
 call allocate_mold(Htot, ne)
 call allocate_mold(HtotG, ne)
@@ -135,6 +134,7 @@ call allocate_mold(psiG, neG)
 call allocate_mold(psi, neG)
 call allocate_mold(tmp, neG)
 call allocate_mold(VeeG, neG)
+call allocate_mold(VenG, neG)
 allocate(X(Ng_local(1), Ng_local(2), Ng_local(3), 3))
 allocate(dpsi(Ng_local(1), Ng_local(2), Ng_local(3), 3))
 call allocate_mold(G, X)
@@ -146,38 +146,67 @@ allocate(Xion(3, natom))
 call assert(abs(L(2)-L(1)) < 1e-15_dp)
 call assert(abs(L(3)-L(1)) < 1e-15_dp)
 !call positions_fcc(Xion, L(1))
-Xion(:,1) = L/2
-Xion(1,1) = Xion(1,1) - 1e-4_dp ! Shift the atom, so that 1/R is defined
+Xion(:,1) = [-0.7_dp+L(1), 0._dp, 0._dp]
+Xion(:,2) = [+0.7_dp     , 0._dp, 0._dp]
+!Xion = Xion + 1e-6_dp ! Shift the atoms, so that 1/R is defined
 call real_space_vectors(L, X, Ng, myxyz)
 call reciprocal_space_vectors(L, G, G2, Ng, myxyz)
 
-do k = 1, Ng_local(3)
-do j = 1, Ng_local(2)
-do i = 1, Ng_local(1)
-    R(i,j,k) = sqrt(sum((X(i,j,k,:)-Xion(:,1))**2))
-end do
-end do
-end do
-!Vloc = R**2 / 2
-!Vloc = -D(5)/R * (D(3)*erf(sqrt(D(1)) * R) + D(4)*erf(sqrt(D(2)) * R))
 ! HDH pseudopotential for Hydrogen
 rloc = 0.2_dp
 C1 = -4.180237_dp
 C2 =  0.725075_dp
 Zion = 1
+
+do k = 1, Ng_local(3)
+do j = 1, Ng_local(2)
+do i = 1, Ng_local(1)
+    w2 = G2(i,j,k)
+    if (w2 < tiny(1._dp)) then
+        Ven0G(i,j,k) = 0
+    else
+        Ven0G(i,j,k) = -4*pi*Zion/w2*exp(-w2*rloc**2/2) &
+            + sqrt(8*pi**3)*rloc**3*exp(-w2*rloc**2/2)*(C1+C2*(3-w2*rloc**2))
+    end if
+end do
+end do
+end do
+Ven0G = Ven0G / product(L)
+
+
+VenG = 0
+do i = 1, natom
+    VenG = VenG - Ven0G * exp(-i_ * &
+        (G(:,:,:,1)*Xion(1,i) + G(:,:,:,2)*Xion(2,i) + G(:,:,:,3)*Xion(3,i)))
+end do
+
+!Vloc = R**2 / 2
+!Vloc = -D(5)/R * (D(3)*erf(sqrt(D(1)) * R) + D(4)*erf(sqrt(D(2)) * R))
 ! He
-C1 = -9.112023_dp
-C2 = 1.698368_dp
-Zion = 2
-Vloc = -Zion/R * erf(R/(sqrt(2._dp)*rloc)) + exp(-1._dp/2*(R/rloc)**2) &
-    * (C1 + C2*(R/rloc)**2)
+!C1 = -9.112023_dp
+!C2 = 1.698368_dp
+!Zion = 2
+!Vloc = 0
+!do na = 1, natom
+!    do k = 1, Ng_local(3)
+!    do j = 1, Ng_local(2)
+!    do i = 1, Ng_local(1)
+!        r = sqrt(sum((X(i,j,k,:)-Xion(:,na))**2))
+!        Vloc(i,j,k) = Vloc(i,j,k) - Zion/r * erf(r/(sqrt(2._dp)*rloc)) &
+!            + exp(-1._dp/2*(r/rloc)**2) * (C1 + C2*(r/rloc)**2)
+!    end do
+!    end do
+!    end do
+!end do
+
+call pfourier2real(VenG, Vloc, commy, commz, Ng, nsub)
 
 Hn = Vloc
 
 if (myid == 0) print *, "Solving eigenproblem: DOFs =", product(Ng)
 
 nev = 2
-ncv = 20
+ncv = 100
 allocate(eigs(nev), orbitals(Ng_local(1),Ng_local(2),Ng_local(3),nev))
 call solve_schroedinger(myid, comm_all, commy, commz, Ng, nsub, Vloc, &
         L, G2, nev, ncv, eigs, orbitals)
@@ -202,8 +231,8 @@ if (myid == 0) then
     end do
 end if
 
-allocate(occ(1))
-occ = [2]
+allocate(occ(2))
+occ = [1, 1]
 do j = 1, 100
 
     ! Poisson
