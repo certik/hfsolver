6ca66088da9e15ea4d6233d8b88a2c95548e09b3
Ondřej Čertík <ondrej.certik@gmail.com>
2017-05-12 14:56:17 -0600

    Check the test output

---
+++ src/tests/fft/tests_ks/1/check_output.py
@@ -0,0 +1,19 @@
+from json import load
+from numpy import array
+
+# Test results
+D = load(open("output.json"))
+eigs = array(D["md"][0]["energies"])
+occ = array(D["md"][0]["occupation"])
+
+# Reference results
+Dref = load(open("output_ref.json"))
+eigs_ref = array(Dref["md"][0]["energies"])
+occ_ref = array(Dref["md"][0]["occupation"])
+
+eig_err = abs(eigs-eigs_ref).max()
+occ_err = abs(occ-occ_ref).max()
+print "Eigs error:", eig_err
+print "Occupation error:", occ_err
+assert eig_err < 1e-12
+assert occ_err < 1e-12
