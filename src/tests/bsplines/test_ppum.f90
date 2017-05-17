d89ffa963ba0e395cfa474499c2631b4a191f0d4
Ondřej Čertík <ondrej.certik@gmail.com>
2017-05-17 14:48:16 -0600

    Calculate enrichment

--- src/tests/bsplines/test_ppum.f90
+++ src/tests/bsplines/test_ppum.f90
@@ -7,21 +7,22 @@ use linalg, only: eigh
 use utils, only: stop_error, str
 implicit none
 
-call do_ppum_basis(3, 0._dp, 1._dp, 4, 1.5_dp)
+call do_ppum_basis(3, 0._dp, 1._dp, 4, 6, 1.5_dp)
 
 contains
 
-    subroutine do_ppum_basis(p, xmin, xmax, Ne, alpha)
-    integer, intent(in) :: p, Ne
+    subroutine do_ppum_basis(p, xmin, xmax, Ne, Nenr, alpha)
+    integer, intent(in) :: p, Ne, Nenr
     real(dp), intent(in) :: xmin, xmax, alpha
     integer :: Nq, N_intervals, Nq_total
     real(dp) :: rmin, rmax, dx
-    real(dp) :: xa, xb, jac
+    real(dp) :: xa, xb, jac, x0
     real(dp), allocatable :: xq(:), wq(:), hq(:), t(:), xiq(:), wtq(:), x(:)
     real(dp), allocatable :: W(:,:), Wp(:,:), Wpp(:,:), S(:), Sp(:), Spp(:)
     real(dp), allocatable :: wi(:,:), wip(:,:), wipp(:,:)
+    real(dp), allocatable :: enr(:,:,:)
     real(dp), allocatable :: mesh(:)
-    integer :: i, u, bindex
+    integer :: i, j, u, bindex
     integer :: n, k
     k = p+1
     if (mod(p, 2) == 0) then
@@ -55,6 +56,7 @@ contains
     allocate(S(Nq_total), wi(Nq_total,Ne))
     allocate(Sp(Nq_total), wip(Nq_total,Ne))
     allocate(Spp(Nq_total), wipp(Nq_total,Ne))
+    allocate(enr(Nq_total,Nenr,Ne))
 
     ! Loop over the mesh, and constract a global quadrature rule. Integrals of a
     ! function hq evaluated at the points xq are calculated using: sum(wq*hq)
@@ -98,6 +100,20 @@ contains
             + 2*W(:,i)*Sp**2)/S**3
     end do
 
+    ! Construct enrichment functions enr(x)
+    do i = 1, Ne
+        dx = (xmax-xmin)/Ne*(alpha-1)/2
+        rmin = (xmax-xmin)/Ne * (i-1) + xmin - dx
+        rmax = (xmax-xmin)/Ne * i + xmin + dx
+        x0 = (rmax-rmin)/2
+        do j = 1, Nenr
+            enr(:,j,i) = ((xq-x0)/(rmax-rmin))**(j-1)
+            where (xq < rmin .or. xq > rmax)
+                enr(:,j,i) = 0
+            end where
+        end do
+    end do
+
 
     open(newunit=u, file="ppum.txt", status="replace")
     write(u,*) xq
@@ -112,6 +128,14 @@ contains
     end do
     do i = 1, Ne
         write(u,*) wipp(:,i)
+    end do
+    close(u)
+
+    open(newunit=u, file="enr.txt", status="replace")
+    do i = 1, Ne
+        do j = 1, Nenr
+            write(u,*) enr(:,j,i)
+        end do
     end do
     close(u)
     end subroutine
