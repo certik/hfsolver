cdecccac41a01bce56c15990368e65b337b33010
Ondřej Čertík <ondrej.certik@gmail.com>
2017-05-16 19:39:09 -0600

    Do the correct basis

--- src/tests/bsplines/test_ppum.f90
+++ src/tests/bsplines/test_ppum.f90
@@ -7,18 +7,21 @@ use linalg, only: eigh
 use utils, only: stop_error, str
 implicit none
 
-call do_basis(5, 4)
+call do_ppum_basis(3)
 
 contains
 
-    subroutine do_basis(n, k)
-    integer, intent(in) :: n, k
+    subroutine do_ppum_basis(p)
+    integer, intent(in) :: p
     integer :: Nq, N_intervals, Nq_total, Nb
     real(dp) :: rmin, rmax, a
     real(dp) :: xa, xb, jac
     real(dp), allocatable :: xq(:), wq(:), hq(:), t(:), xiq(:), wtq(:), x(:)
     real(dp), allocatable :: B(:,:), Bp(:,:), Bpp(:,:)
     integer :: i, u
+    integer :: n, k
+    k = p+1
+    n = k+1
     print *, "Constructing B-spline basis n =", n, ", k =", k
     Nq = 64
     N_intervals = n-k+1
@@ -29,10 +32,9 @@ contains
     allocate(xq(Nq_total), wq(Nq_total), hq(Nq_total))
     allocate(B(Nq_total, n), Bp(Nq_total, n), Bpp(Nq_total, n))
 
-    rmin = -1
+    rmin = 0
     rmax = 1
     a = 1
-
     t(:k-1) = rmin
     t(k:n+1) = meshexp(rmin, rmax, a, N_intervals)
     t(n+2:) = rmax
@@ -51,20 +53,46 @@ contains
         wq((i-1)*Nq+1:i*Nq) = wtq*jac
     end do
 
+
+    open(newunit=u, file="ppum.txt", status="replace")
+    write(u,*) xq
+
+    rmin = 0
+    rmax = 5._dp/12
+    t(:k-1) = rmin
+    t(k:n+1) = meshexp(rmin, rmax, a, N_intervals)
+    t(n+2:) = rmax
     do i = 1, Nb
         B(:,i)   = bspline     (t, i, k, xq)
         Bp(:,i)  = bspline_der (t, i, k, xq)
         Bpp(:,i) = bspline_der2(t, i, k, xq)
     end do
+    write(u,*) B(:,Nb/2+1)
 
-    open(newunit=u, file="ppum.txt", status="replace")
-    write(u,*) t
-    write(u,*) xq
-    do i = 1, n
-        write(u,*) B(:,i)
-        write(u,*) Bp(:,i)
-        write(u,*) Bpp(:,i)
+    rmin = 1._dp/4
+    rmax = 3._dp/4
+    t(:k-1) = rmin
+    t(k:n+1) = meshexp(rmin, rmax, a, N_intervals)
+    t(n+2:) = rmax
+    do i = 1, Nb
+        B(:,i)   = bspline     (t, i, k, xq)
+        Bp(:,i)  = bspline_der (t, i, k, xq)
+        Bpp(:,i) = bspline_der2(t, i, k, xq)
     end do
+    write(u,*) B(:,Nb/2+1)
+
+    rmin = 7._dp/12
+    rmax = 1
+    t(:k-1) = rmin
+    t(k:n+1) = meshexp(rmin, rmax, a, N_intervals)
+    t(n+2:) = rmax
+    do i = 1, Nb
+        B(:,i)   = bspline     (t, i, k, xq)
+        Bp(:,i)  = bspline_der (t, i, k, xq)
+        Bpp(:,i) = bspline_der2(t, i, k, xq)
+    end do
+    write(u,*) B(:,Nb/2+1)
+
     close(u)
     end subroutine
 
