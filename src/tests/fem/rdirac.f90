dc22c2f4f50d79df36cfb14d3c95eac790d7b65c
Ondřej Čertík <ondrej.certik@gmail.com>
2017-06-09 14:25:35 -0600

    Add dirac

--- src/tests/fem/rdirac.f90
+++ src/tests/fem/rdirac.f90
@@ -3,7 +3,7 @@ use types, only: dp
 use sorting, only: sort
 use constants, only: pi
 use feutils, only: get_parent_nodes, get_parent_quad_pts_wts, phih, dphih, &
-    get_nodes, define_connect, c2fullc => c2fullc2, get_quad_pts, fe2quad
+    get_nodes, define_connect_n, c2fullc => c2fullc2, get_quad_pts, fe2quad
 use utils, only: assert, stop_error
 use splines, only: iixmin, spline3pars, poly3, dpoly3
 use mesh, only: meshexp
@@ -15,8 +15,8 @@ public sfem
 contains
 
 subroutine sfem(Ne, p, Nq, L, Nb, kappa, a_, c, Z, eigs)
-integer, intent(in) :: Ne, p, Nq
-real(dp), intent(in) :: L, kappa, a_, c, Z
+integer, intent(in) :: Ne, p, Nq, kappa
+real(dp), intent(in) :: L, a_, c, Z
 integer, intent(out) :: Nb
 real(dp), allocatable, intent(out) :: eigs(:)
 
@@ -25,11 +25,11 @@ integer :: Nn
 real(dp), allocatable :: xe(:)
 real(dp), allocatable :: xin(:), xiq(:), wtq(:), A(:, :), B(:, :), sol(:, :), &
     phihq(:, :), dphihq(:, :), Vq(:,:), xn(:), &
-    fullc(:), enrq(:,:,:), denrq(:,:,:), phipuq(:,:), dphipuq(:,:), xinpu(:), &
+    fullc(:), phipuq(:,:), dphipuq(:,:), xinpu(:), &
     xq(:,:)
-integer, allocatable :: ib(:, :), in(:, :), ibenr(:,:,:)
-real(dp) :: rc, En
-integer :: i, j, iqx, u, Nenr, emin, emax, l_, relat
+integer, allocatable :: ib(:,:,:), in(:,:,:)
+real(dp) :: En
+integer :: i, j, l_, relat
 
 Nn = Ne*p+1
 allocate(xe(Ne+1))
@@ -54,8 +54,8 @@ forall(i=1:size(xiq), j=1:size(xin)) dphihq(i, j) = dphih(xin, j, xiq(i))
 forall(i=1:size(xiq), j=1:size(xinpu))  phipuq(i, j) =  phih(xinpu, j, xiq(i))
 forall(i=1:size(xiq), j=1:size(xinpu)) dphipuq(i, j) = dphih(xinpu, j, xiq(i))
 
-allocate(in(p+1,Ne),ib(p+1,Ne))
-call define_connect(1,1,Ne,p,in,ib)
+allocate(in(p+1,Ne,2),ib(p+1,Ne,2))
+call define_connect_n([1,2],[1,2],Ne,p,2,in,ib)
 
 Nb = maxval(ib)
 
@@ -64,11 +64,11 @@ allocate(fullc(Nn))
 
 Vq = -Z/xq
 
-call assemble_1d(xin, xe, ib, xiq, wtq, phihq, dphihq, Vq, A, B)
+call assemble_rdirac_squared(Vq,c,kappa,xin,xe,ib,xiq,wtq,A,B)
 call eigh(A, B, eigs, sol)
 
 print *, "n, energy, exact energy, error"
-do i = 1, Nb
+do i = 1, min(20,Nb)
     if (kappa > 0) then
         l_ = kappa
         relat = 3
@@ -109,61 +109,115 @@ else
 end if
 end function
 
-subroutine assemble_1d(xin, nodes, ib, xiq, wtq, phihq, dphihq, Vq, Am, Bm)
-! Assemble on a 1D uniform mesh
-real(dp), intent(in):: xin(:), nodes(:), xiq(:), wtq(:), &
-    phihq(:, :), dphihq(:, :), Vq(:,:)
-integer, intent(in):: ib(:, :)
-real(dp), intent(out):: Am(:,:), Bm(:, :)
-real(dp), dimension(size(xiq), &
-    size(xin)) :: phi_v, phi_dx
-real(dp), dimension(size(xiq)) :: x, xp
-integer :: Ne, p, e, i, j, iqx
-real(dp) :: lx
-integer :: ax, bx
-real(dp) :: jacx, jac_det
 
-Ne = size(nodes)-1
-p = size(xin) - 1
-! 1D shape functions
-do ax = 1, p+1
-    do iqx = 1, size(xiq)
-        phi_v (iqx, ax) =  phihq(iqx, ax)
-        phi_dx(iqx, ax) = dphihq(iqx, ax)
-    end do
+subroutine assemble_rdirac_squared(V,c,kappa,xin,xe,ib,xiq,wtq,Am,Bm)
+! forms system equation matrices corresponding to the Dirac eigenproblem
+! subject to boundary conditions consistent with basis specified by ib
+! Both Dirac components are on the same mesh.
+real(dp), intent(in) :: V(:,:)   ! V(x) at quadrature points:
+   ! V(i,j) = value at ith point in jth element
+real(dp), intent(in) :: c ! speed of light
+integer, intent(in) :: kappa     ! kappa
+real(dp), intent(in):: xin(:)       ! parent basis nodes
+real(dp), intent(in):: xe(:)
+integer, intent(in):: ib(:,:,:)     ! basis connectivity: ib(i,j,k) = index of
+    ! basis function associated with local basis function i of element j of
+    ! mesh k. 0 = no associated basis fn.
+real(dp), intent(in):: xiq(:)       ! quadrature points
+real(dp), intent(in):: wtq(:)       ! quadrature weights
+real(dp), intent(out):: Am(:,:)     ! LHS matrix: Am c = lam Bm c
+real(dp), intent(out):: Bm(:,:)     ! RHS matrix: Am c = lam Bm c
+integer Ne, Nb              ! number of elements, nodes, basis functions
+integer p                     ! order of FE/SE basis
+integer e                     ! element index
+integer i,j                   ! basis fn indices
+integer :: m1, m2
+integer al,be                 ! "alpha", "beta": local basis fn indices
+integer iq                    ! quadrature point index
+real(dp) xa,xb                ! element boundary node coordinates
+real(dp) jac                  ! Jacobian of transformation from parent coords xi in [-1,1]
+                              ! to coords x in [xa,xb]: x = (xb-xa)/2 xi + (xb+xa)/2
+real(dp) phihq(size(xiq),size(xin)),dphihq(size(xiq),size(xin))   ! parent basis fns and derivs
+   ! at quadrature points: phihq(i,j) = value of jth function at ith quadrature point
+real(dp) Vq(size(xiq))
+real(dp) Vqx(size(xiq))
+real(dp) hq(size(xiq))        ! integrand at quadrature points
+real(dp) x(size(xiq))                    ! point in domain
+real(dp) :: n, k
+real(dp), dimension(size(xiq)) :: Bi, Bj, Bip, Bjp
+real(dp) :: l, lb
+
+n = 0
+k = 1000
+
+! initializations
+Ne= size(xe) - 1
+Nb=maxval(ib)
+p=size(xin)-1
+if (size(Am,1)/=Nb .or. size(Bm,1)/=Nb) call stop_error("Error: size of Am and/or Bm inconsistent with Nb.")
+! tabulate parent basis and derivatives at quadrature points
+do al = 1, p+1
+   do iq =1, size(xiq)
+      phihq(iq, al) = phih(xin, al, xiq(iq))
+      dphihq(iq, al) = dphih(xin, al, xiq(iq))
+   end do
 end do
+
+! accumulate Am and Bm matrix elements
 Am=0; Bm=0
-! Precalculate as much as possible:
-lx = nodes(2) - nodes(1) ! Element size
-jacx = lx/2
-jac_det = abs(jacx)
-xp = (xiq + 1) * jacx
-phi_dx = phi_dx / jacx
-do e = 1, Ne
-    x = xp + nodes(e)
-    do bx = 1, p+1
-        j = ib(bx, e)
-        if (j==0) cycle
-        do ax = 1, p+1
-            i = ib(ax, e)
-            if (i == 0) cycle
-            if (j > i) cycle
-            Am(i,j) = Am(i,j) + sum(phi_dx(:, ax)*phi_dx(:, bx) &
-                * jac_det * wtq) / 2
-            Am(i,j) = Am(i,j) + sum(Vq(:,e) * &
-                phi_v(:, ax)*phi_v(:, bx) &
-                * jac_det * wtq)
-            Bm(i,j) = Bm(i,j) + sum(( &
-                phi_v(:, ax) * phi_v(:, bx) &
-                * jac_det * wtq))
+do m1 = 1, 2
+    do m2 = 1, 2
+        do e = 1, Ne
+            xa = xe(e)
+            xb = xe(e+1)
+            jac = (xb-xa)/2  ! affine mapping
+            x = (xiq(:)+1) * jac + xa ! affine mapping
+            Vq = V(:, e)
+            Vqx = Vq*x ! Potential times r
+            ! compute matrix elements (integrals transformed to [-1,1])
+            do be = 1, p+1
+                j = ib(be, e, m2)
+                if (j == 0) cycle    ! omit boundary basis fns for Dirichlet BCs
+                do al = 1, p+1
+                    i = ib(al, e, m1)
+                    if (i == 0) cycle! omit boundary basis fns for Dirichlet BCs
+                    Bi = phihq(:,al)
+                    Bj = phihq(:,be)
+                    Bip = dphihq(:,al)/jac
+                    Bjp = dphihq(:,be)/jac
+                    l = kappa
+                    lb = -kappa
+                    if (m1 == 1 .and. m2 == 1) then
+                        hq = c**2*Bip*Bjp &
+                            + Bi*Bj*((Vq+c**2)**2+c**2*(kappa*(kappa+1)/x**2))
+                    else if (m1 == 1 .and. m2 == 2) then
+                        hq = c*Vq*(+Bip*Bj-Bi*Bjp + 2*kappa/x*Bi*Bj)
+                    else if (m1 == 2 .and. m2 == 1) then
+                        hq = c*Vq*(-Bip*Bj+Bi*Bjp + 2*kappa/x*Bi*Bj)
+                    else if (m1 == 2 .and. m2 == 2) then
+                        hq = c**2*Bip*Bjp &
+                            + Bi*Bj*((Vq-c**2)**2+c**2*(-kappa*(-kappa+1)/x**2))
+                    end if
+                    Am(i,j) = Am(i,j) + sum(wtq*hq*jac)
+                    if (m1 == m2) then
+                        Bm(i,j) = Bm(i,j) + sum(wtq*Bi*Bj*jac)
+                    end if
+                end do
+            end do
         end do
     end do
 end do
-do j = 1, size(Am, 2)
-    do i = 1, j-1
-        Am(i, j) = Am(j, i)
-        Bm(i, j) = Bm(j, i)
-    end do
+! check symmetry
+print *, "Checking symmetry"
+do j=1,Nb
+    do i=1,j-1
+        if (abs(Am(i,j)-Am(j,i)) / (max(Am(i,j), Am(j,i))+tiny(1._dp)) &
+                > 1e-8_dp) then
+            print *, i, j, Am(i,j)-Am(j,i), Am(i,j), Am(j,i)
+            call stop_error("Am not symmetric")
+        end if
+        if (abs(Bm(i,j)-Bm(j,i)) > 1e-12_dp) call stop_error("Bm not symmetric")
+   end do
 end do
 end subroutine
 
@@ -179,9 +233,9 @@ use types, only: dp
 use rdirac_assembly, only: sfem
 implicit none
 
-integer :: Ne, p, Nq, DOFs, i, u
+integer :: Ne, p, Nq, DOFs, i, kappa
 real(dp), allocatable :: eigs(:)
-real(dp) :: L, a, c, kappa, Z
+real(dp) :: L, a, c, Z
 
 Ne = 4
 p = 10
