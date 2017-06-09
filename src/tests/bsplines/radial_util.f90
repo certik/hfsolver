b33bcd6437dceb00fb201b3d54372874d82653c1
Ondřej Čertík <ondrej.certik@gmail.com>
2017-06-09 12:34:48 -0600

    Refactor test_dirac into radial_util

--- src/tests/bsplines/radial_util.f90
+++ src/tests/bsplines/radial_util.f90
@@ -4,7 +4,7 @@ use linalg, only: eigh, eigvals
 use utils, only: stop_error
 implicit none
 private
-public lho, radial
+public lho, radial, radial_dirac
 
 contains
 
@@ -115,5 +115,133 @@ do i = 1, min(Nb, 20)
     print "(i4, f30.8, f18.8, es12.2)", i, lam(i), En, abs(lam(i)-En)
 end do
 end subroutine
+
+
+subroutine radial_dirac(Nb, xq, wq, B, Bp, Z, kappa, c)
+integer, intent(in) :: Nb, Z, kappa
+real(dp), intent(in) :: xq(:), wq(:),  B(:,:), Bp(:,:), c
+real(dp), allocatable :: Am(:,:), Bm(:,:), sol(:,:), lam(:), hq(:)
+real(dp), allocatable :: Bi(:), Bj(:), Bip(:), Bjp(:)
+real(dp), allocatable :: Vq(:), Vqp(:)
+real(dp) :: En
+integer :: i, j, l, relat, Nq_total
+Nq_total = size(xq)
+
+allocate(Am(2*Nb,2*Nb), Bm(2*Nb,2*Nb), sol(2*Nb,2*Nb), lam(2*Nb))
+allocate(hq(Nq_total))
+allocate(Bi(Nq_total), Bj(Nq_total), Bip(Nq_total), Bjp(Nq_total), Vq(Nq_total))
+allocate(Vqp(Nq_total))
+
+print *, "Assembly"
+! Construct matrices A and B
+do i = 1, Nb
+    do j = 1, Nb
+        Bi = B(:,i)
+        Bj = B(:,j)
+        Bip = Bp(:,i)
+        Bjp = Bp(:,j)
+        Vq = -Z/xq
+        Vqp = Z/xq**2
+        ! A11
+        hq = -c**2*(-Bip*xq**2*Bjp -kappa*(kappa+1)*Bi*Bj) &
+            +c**4*Bi*xq**2*Bj+Bi*xq**2*Vq**2*Bj+2*c**2*Bi*xq**2*Vq*Bj
+        Am(i,j) = sum(wq*hq)
+        ! A12
+        hq = -c*(2*Bi*xq**2*Vq*Bjp+2*(1-kappa)*Bi*xq*Vq*Bj+Bi*xq**2*Vqp*Bj)
+        Am(i,j+Nb) = sum(wq*hq)
+        ! A21
+        hq = c*(2*Bi*xq**2*Vq*Bjp+2*(1+kappa)*Bi*xq*Vq*Bj+Bi*xq**2*Vqp*Bj)
+        Am(i+Nb,j) = sum(wq*hq)
+        ! A22
+        hq = -c**2*(-Bip*xq**2*Bjp -(-kappa)*(-kappa+1)*Bi*Bj) &
+            +c**4*Bi*xq**2*Bj+Bi*xq**2*Vq**2*Bj-2*c**2*Bi*xq**2*Vq*Bj
+        Am(i+Nb,j+Nb) = sum(wq*hq)
+
+        ! B11
+        hq = B(:,i)*B(:,j)*xq**2
+        Bm(i,j) = sum(wq*hq)
+        ! B22
+        Bm(i+Nb,j+Nb) = Bm(i,j)
+    end do
+end do
+
+print *, "Checking symmetry"
+do j = 1, 2*Nb
+    do i = 1, j-1
+        if (max(abs(Am(i,j)), abs(Am(j,i))) > tiny(1._dp)) then
+            if (abs(Am(i,j)-Am(j,i)) / max(abs(Am(i,j)), abs(Am(j,i))) &
+                    > 1e-8_dp) then
+                print *, i, j, Am(i,j)-Am(j,i), Am(i,j), Am(j,i)
+                call stop_error("Am not symmetric")
+            end if
+        end if
+        if (abs(Bm(i,j)-Bm(j,i)) > 1e-12_dp) call stop_error("Bm not symmetric")
+   end do
+end do
+
+
+print *, "Eigensolver"
+! Solve an eigenproblem
+call eigh(Am, Bm, lam, sol)
+
+print *, "n, energy, exact energy, error"
+do i = 1, Nb
+    if (kappa > 0) then
+        l = kappa
+        relat = 3
+    else
+        l = -kappa-1
+        relat = 2
+    end if
+    En = E_nl(c, l+i, l, real(Z, dp), relat)
+    lam(i) = sqrt(lam(i)) - c**2
+    print "(i4, f30.8, f18.8, es12.2)", i, lam(i), En, abs(lam(i)-En)
+end do
+
+!do i = 1, Nb
+!    solsP(:,i) = 0
+!    do j = 1, Nb
+!        solsP(:,i) = solsP(:,i) + sol(j,i)*B(:,j)
+!    end do
+!end do
+!
+!open(newunit=u, file="dirac_sol.txt", status="replace")
+!write(u,*) xq
+!do i = 1, Nb
+!    write(u,*) solsP(:,i)
+!end do
+!close(u)
+
+contains
+
+    real(dp) function E_nl(c, n, l, Z, relat)
+    ! Calculates exact energy for the radial Schroedinger/Dirac equations
+    real(dp), intent(in) :: c, Z ! speed of light in atomic units
+    integer, intent(in) :: n, l, relat
+    ! quantum numbers (n, l), atomic number (z)
+    ! relat == 0 ... Schroedinger equation
+    ! relat == 2 ... Dirac equation, spin up
+    ! relat == 3 ... Dirac equation, spin down
+
+    integer :: skappa
+    real(dp) :: beta
+    if (.not. (l >= 0)) call stop_error("'l' must be positive or zero")
+    if (.not. (n > l)) call stop_error("'n' must be greater than 'l'")
+    if (l == 0 .and. relat == 3) call stop_error("Spin must be up for l==0.")
+    if (relat == 0) then
+        E_nl = - Z**2 / (2.0_dp * n**2)
+    else
+        if (relat == 2) then
+            skappa = -l - 1
+        else
+            skappa = -l
+        end if
+        beta = sqrt(skappa**2 - Z**2 / c**2)
+        E_nl = c**2/sqrt(1+Z**2/(n + skappa + beta)**2/c**2) - c**2
+    end if
+    end function
+
+end subroutine
+
 
 end module
--- src/tests/bsplines/test_dirac.f90
+++ src/tests/bsplines/test_dirac.f90
@@ -5,6 +5,7 @@ use mesh, only: meshexp
 use quadrature, only: gauss_pts, gauss_wts
 use linalg, only: eigh
 use utils, only: stop_error
+use radial_util, only: radial_dirac
 implicit none
 
 integer, parameter :: n = 30, k = 6, Nq=7
@@ -12,22 +13,13 @@ integer, parameter :: N_intervals = n-k+1
 integer, parameter :: Nq_total = Nq*N_intervals, Nb=n-1
 real(dp) :: t(n+k), rmin, rmax, a
 real(dp) :: xiq(Nq), wtq(Nq), xa, xb, jac, x(Nq)
-real(dp), allocatable :: xq(:), wq(:), hq(:)
-real(dp), allocatable :: B(:,:), Bp(:,:), Bpp(:,:), Bi(:), Bj(:), Bip(:), Bjp(:)
-real(dp), allocatable :: Bipp(:), Bjpp(:)
-real(dp), allocatable :: Vq(:), Vqp(:)
-real(dp), allocatable :: Am(:,:), Bm(:,:), sol(:,:), lam(:)
-real(dp), allocatable :: solsP(:,:), solsQ(:,:)
-real(dp) :: En, c
-integer :: i, j, kappa, Z, l, relat, u
+real(dp), allocatable :: xq(:), wq(:)
+real(dp), allocatable :: B(:,:), Bp(:,:), Bpp(:,:)
+real(dp) :: c
+integer :: i, kappa, Z
 
-allocate(Am(2*Nb,2*Nb), Bm(2*Nb,2*Nb), sol(2*Nb,2*Nb), lam(2*Nb))
-allocate(xq(Nq_total), wq(Nq_total), hq(Nq_total))
+allocate(xq(Nq_total), wq(Nq_total))
 allocate(B(Nq_total, Nb), Bp(Nq_total, Nb), Bpp(Nq_total, Nb))
-allocate(Bi(Nq_total), Bj(Nq_total), Bip(Nq_total), Bjp(Nq_total), Vq(Nq_total))
-allocate(Bipp(Nq_total), Bjpp(Nq_total), Vqp(Nq_total))
-allocate(solsP(Nq_total,Nb))
-allocate(solsQ(Nq_total,Nb))
 
 rmin = 1e-15_dp
 rmax = 10
@@ -71,115 +63,6 @@ do i = 1, Nb
     Bpp(:,i) = bspline_der2(t, i, k, xq)
 end do
 
-print *, "Assembly"
-! Construct matrices A and B
-do i = 1, Nb
-    do j = 1, Nb
-        Bi = B(:,i)
-        Bj = B(:,j)
-        Bip = Bp(:,i)
-        Bjp = Bp(:,j)
-        Bipp = Bpp(:,i)
-        Bjpp = Bpp(:,j)
-        Vq = -Z/xq
-        Vqp = Z/xq**2
-        ! A11
-        hq = -c**2*(-Bip*xq**2*Bjp -kappa*(kappa+1)*Bi*Bj) &
-            +c**4*Bi*xq**2*Bj+Bi*xq**2*Vq**2*Bj+2*c**2*Bi*xq**2*Vq*Bj
-        Am(i,j) = sum(wq*hq)
-        ! A12
-        hq = -c*(2*Bi*xq**2*Vq*Bjp+2*(1-kappa)*Bi*xq*Vq*Bj+Bi*xq**2*Vqp*Bj)
-        Am(i,j+Nb) = sum(wq*hq)
-        ! A21
-        hq = c*(2*Bi*xq**2*Vq*Bjp+2*(1+kappa)*Bi*xq*Vq*Bj+Bi*xq**2*Vqp*Bj)
-        Am(i+Nb,j) = sum(wq*hq)
-        ! A22
-        hq = -c**2*(-Bip*xq**2*Bjp -(-kappa)*(-kappa+1)*Bi*Bj) &
-            +c**4*Bi*xq**2*Bj+Bi*xq**2*Vq**2*Bj-2*c**2*Bi*xq**2*Vq*Bj
-        Am(i+Nb,j+Nb) = sum(wq*hq)
-
-        ! B11
-        hq = B(:,i)*B(:,j)*xq**2
-        Bm(i,j) = sum(wq*hq)
-        ! B22
-        Bm(i+Nb,j+Nb) = Bm(i,j)
-    end do
-end do
-
-print *, "Checking symmetry"
-do j = 1, 2*Nb
-    do i = 1, j-1
-        if (max(abs(Am(i,j)), abs(Am(j,i))) > tiny(1._dp)) then
-            if (abs(Am(i,j)-Am(j,i)) / max(abs(Am(i,j)), abs(Am(j,i))) &
-                    > 1e-8_dp) then
-                print *, i, j, Am(i,j)-Am(j,i), Am(i,j), Am(j,i)
-                call stop_error("Am not symmetric")
-            end if
-        end if
-        if (abs(Bm(i,j)-Bm(j,i)) > 1e-12_dp) call stop_error("Bm not symmetric")
-   end do
-end do
-
-
-print *, "Eigensolver"
-! Solve an eigenproblem
-call eigh(Am, Bm, lam, sol)
-
-print *, "n, energy, exact energy, error"
-do i = 1, Nb
-    if (kappa > 0) then
-        l = kappa
-        relat = 3
-    else
-        l = -kappa-1
-        relat = 2
-    end if
-    En = E_nl(c, l+i, l, real(Z, dp), relat)
-    lam(i) = sqrt(lam(i)) - c**2
-    print "(i4, f30.8, f18.8, es12.2)", i, lam(i), En, abs(lam(i)-En)
-end do
-
-do i = 1, Nb
-    solsP(:,i) = 0
-    do j = 1, Nb
-        solsP(:,i) = solsP(:,i) + sol(j,i)*B(:,j)
-    end do
-end do
-
-open(newunit=u, file="dirac_sol.txt", status="replace")
-write(u,*) xq
-do i = 1, Nb
-    write(u,*) solsP(:,i)
-end do
-close(u)
-
-contains
-
-real(dp) function E_nl(c, n, l, Z, relat)
-! Calculates exact energy for the radial Schroedinger/Dirac equations
-real(dp), intent(in) :: c, Z ! speed of light in atomic units
-integer, intent(in) :: n, l, relat
-! quantum numbers (n, l), atomic number (z)
-! relat == 0 ... Schroedinger equation
-! relat == 2 ... Dirac equation, spin up
-! relat == 3 ... Dirac equation, spin down
-
-integer :: skappa
-real(dp) :: beta
-if (.not. (l >= 0)) call stop_error("'l' must be positive or zero")
-if (.not. (n > l)) call stop_error("'n' must be greater than 'l'")
-if (l == 0 .and. relat == 3) call stop_error("Spin must be up for l==0.")
-if (relat == 0) then
-    E_nl = - Z**2 / (2.0_dp * n**2)
-else
-    if (relat == 2) then
-        skappa = -l - 1
-    else
-        skappa = -l
-    end if
-    beta = sqrt(skappa**2 - Z**2 / c**2)
-    E_nl = c**2/sqrt(1+Z**2/(n + skappa + beta)**2/c**2) - c**2
-end if
-end function
+call radial_dirac(Nb, xq, wq, B, Bp, Z, kappa, c)
 
 end program
